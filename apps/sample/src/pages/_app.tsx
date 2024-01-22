2c60f5422bbf390493cdebe725d9a5680c8c26f5
Louis Aussedat <louis.aussedat@ledger.fr>
2024-01-22 12:03:55 +0100

    fix(sample): add Inner google font

--- apps/sample/src/pages/_app.tsx
+++ apps/sample/src/pages/_app.tsx
@@ -1,14 +1,17 @@
 import { CustomThemeProvider } from "@/providers/theme";
 import { GlobalStyle } from "@/styles/globalstyles";
 import type { AppProps } from "next/app";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <main className={inter.className}>
       <CustomThemeProvider>
         <GlobalStyle />
         <Component {...pageProps} />
       </CustomThemeProvider>
-    </>
+    </main>
   );
 }
--- apps/sample/src/styles/globalstyles.tsx
+++ apps/sample/src/styles/globalstyles.tsx
@@ -3,12 +3,12 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyle = createGlobalStyle`
   html,
   body,
-  #__next {
+  #__next,
+  main {
     width: 100%;
     height: 100%;
     padding: 0;
     margin: 0;
     background-color: #000000;
-    font-family: Inter;
   }
 `;
