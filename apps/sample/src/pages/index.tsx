62381ca283d97b81e3e04755c3f5cfb0005e3fdb
Louis Aussedat <louis.aussedat@ledger.fr>
2024-01-30 17:00:38 +0100

    fix(sample): downgrade styled-components to match react-ui ledgerh

--- apps/sample/src/pages/index.tsx
+++ apps/sample/src/pages/index.tsx
@@ -1,13 +1,14 @@
 "use client";
 import React from "react";
 import { Box } from "@ledgerhq/react-ui/index";
-import styled from "styled-components";
+import styled, { DefaultTheme } from "styled-components";
 
 const MainContainer = styled(Box)`
   width: 100%;
   height: 100%;
-  background-color: ${({ theme }) => theme.colors.background.main};
-  color: ${({ theme }) => theme.colors.neutral.c90};
+  background-color: ${({ theme }: { theme: DefaultTheme }) =>
+    theme.colors.background.main};
+  color: ${({ theme }: { theme: DefaultTheme }) => theme.colors.neutral.c90};
 `;
 
 const Home: React.FC = () => {
