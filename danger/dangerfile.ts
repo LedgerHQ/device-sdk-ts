f6214fbdcbcedf1ea7ebac81508fe0b163c5d23a
Louis Aussedat <louis.aussedat@ledger.fr>
2024-02-23 09:21:55 +0100

    ✏️ (chore): Add link to the documentation in case of danger fail

--- danger/dangerfile.ts
+++ danger/dangerfile.ts
@@ -9,18 +9,24 @@ if (danger.github.pr.assignee === null) {
 const branchRegex =
   /^(feature|bugfix|support|chore|doc)\/((DSDK|dsdk)-[0-9]+|NO-ISSUE|no-issue)\-.*/;
 if (!branchRegex.test(danger.github.pr.head.ref)) {
-  fail("Please fix the PR branch name to match the convention");
+  fail(
+    "Please fix the PR branch name to match the convention, see [this documentation](https://ledgerhq.atlassian.net/wiki/spaces/WXP/pages/4527358147/DSDK+TS+Git+-+Github+conventions)"
+  );
 }
 
 const titleRegex = /^.+ \([a-z]+\) \[(DSDK-[0-9]+|NO-ISSUE)\]: [A-Z].*/;
 if (!titleRegex.test(danger.github.pr.title)) {
-  fail("Please fix the PR title to match the convention.");
+  fail(
+    "Please fix the PR title to match the convention, see [this documentation](https://ledgerhq.atlassian.net/wiki/spaces/WXP/pages/4527358147/DSDK+TS+Git+-+Github+conventions)"
+  );
 }
 
 const commitRegex = /^.+ \([a-z]+\): [A-Z].*/;
 for (let { commit } of danger.github.commits) {
   if (!commitRegex.test(commit.message)) {
-    fail("One or more commit message does not match the convention.");
+    fail(
+      "One or more commit message does not match the convention, see [this documentation](https://ledgerhq.atlassian.net/wiki/spaces/WXP/pages/4527358147/DSDK+TS+Git+-+Github+conventions)."
+    );
     break;
   }
 }
