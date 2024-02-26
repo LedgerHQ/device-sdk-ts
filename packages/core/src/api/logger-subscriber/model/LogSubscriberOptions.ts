29386b2276b4afed3d7d39df4927167e6e14f64a
Louis Aussedat <louis.aussedat@ledger.fr>
2024-02-26 15:21:50 +0100

    🏷️ (core): Remove log context

--- packages/core/src/api/logger-subscriber/model/LogSubscriberOptions.ts
+++ packages/core/src/api/logger-subscriber/model/LogSubscriberOptions.ts
@@ -1,14 +1,7 @@
-export type LogSubscriberContext = Partial<{
-  type: string;
-  tag: string;
-  [key: string]: unknown;
-}>;
-
 export type LogSubscriberData = Record<string, unknown>;
 
 export type LogSubscriberOptions = {
   tag: string;
   timestamp: number;
   data?: LogSubscriberData;
-  context?: LogSubscriberContext;
 };
--- packages/core/src/internal/logger-publisher/model/LogPublisherOptions.ts
+++ packages/core/src/internal/logger-publisher/model/LogPublisherOptions.ts
@@ -1,14 +1,7 @@
-export type LogPublisherContext = Partial<{
-  type: string;
-  tag: string;
-  [key: string]: unknown;
-}>;
-
 export type LogPublisherData = Record<string, unknown>;
 
 export type LogPublisherOptions = {
   tag?: string;
   timestamp?: number;
   data?: LogPublisherData;
-  context?: LogPublisherContext;
 };
