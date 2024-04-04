0eb35f7f3b74baf070b3d3e613265dbd1f3ad25a
Valentin D. Pinkman <valentin.d.pinkman@hey.com>
2024-04-04 18:52:07 +0200

    🔨 (packages): Update the bundling of the libraries

---
+++ packages/config/esbuild/index.js
@@ -0,0 +1,9 @@
+const alias = require("./plugins/alias");
+const copy = require("./plugins/copy");
+const buildConfig = require("./config/common");
+
+module.exports = {
+  alias,
+  copy,
+  buildConfig,
+};
---
+++ packages/signer/scripts/build.mjs
@@ -0,0 +1,69 @@
+#!/usr/bin/env zx
+import { build } from "esbuild";
+import { /* alias, */ buildConfig } from "@ledgerhq/esbuild-tools";
+import dtsPlugin from "esbuild-plugin-d.ts";
+
+const root = path.join(__dirname, "..");
+const srcRoot = path.join(root, "src");
+const outdir = path.join(root, "lib");
+const tsconfigEsm = path.join(root, "tsconfig.esm.json");
+const tsconfigCjs = path.join(root, "tsconfig.cjs.json");
+
+const pkg = fs.readFileSync(path.join(root, "package.json"), "utf-8");
+const packageJson = JSON.parse(pkg);
+
+const conf = {
+  entryPoints: [path.join(srcRoot, "index.ts")],
+  plugins: [
+    // EXAMPLE USAGE, UNCOMMENT TO USE
+    // alias({
+    //   "@root": root,
+    //   "@internal": path.join(srcRoot, "internal"),
+    //   "@api": path.join(srcRoot, "api"),
+    // }),
+  ],
+};
+
+const common = buildConfig(conf, packageJson);
+
+const buildEsm = async () => {
+  const config = {
+    ...common,
+    outdir: path.join(outdir, "esm"),
+    format: "esm",
+    tsconfig: tsconfigEsm,
+    plugins: [
+      ...common.plugins,
+      dtsPlugin({
+        tsconfig: tsconfigEsm,
+      }),
+    ],
+  };
+
+  await build(config);
+};
+
+const builCjs = async () => {
+  const config = {
+    ...common,
+    outdir: path.join(outdir, "cjs"),
+    format: "cjs",
+    tsconfig: tsconfigCjs,
+    plugins: [
+      ...common.plugins,
+      dtsPlugin({
+        tsconfig: tsconfigCjs,
+      }),
+    ],
+  };
+
+  await build(config);
+};
+
+const run = async () => Promise.all([buildEsm(), builCjs()]);
+
+run()
+  .then(() => console.log("success"))
+  .catch((e) => {
+    console.error(e);
+  });
