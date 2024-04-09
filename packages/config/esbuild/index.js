e0e059b53b1705520a37229978e2282f2944eba1
Valentin D. Pinkman <valentin.d.pinkman@hey.com>
2024-04-09 16:30:01 +0200

    🔨 (packages): Do not bundle and export types correctly

--- packages/config/esbuild/index.js
+++
@@ -1,9 +0,0 @@
-const alias = require("./plugins/alias");
-const copy = require("./plugins/copy");
-const buildConfig = require("./config/common");
-
-module.exports = {
-  alias,
-  copy,
-  buildConfig,
-};
