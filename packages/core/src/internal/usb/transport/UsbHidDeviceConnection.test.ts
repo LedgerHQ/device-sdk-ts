0318a9a8660e46f0ac8739b1755e31ebe2a4b5f5
jdabbech-ledger <jalil.dabbech@ledger.com>
2024-04-09 16:12:55 +0200

    :white_check_mark: (core): Improve coverage

--- packages/core/src/internal/usb/transport/UsbHidDeviceConnection.test.ts
+++ packages/core/src/internal/usb/transport/UsbHidDeviceConnection.test.ts
@@ -6,6 +6,14 @@ import { DefaultLoggerPublisherService } from "@internal/logger-publisher/servic
 import { hidDeviceStubBuilder } from "@internal/usb/model/HIDDevice.stub";
 import { UsbHidDeviceConnection } from "@internal/usb/transport/UsbHidDeviceConnection";
 
+const RESPONSE_LOCKED_DEVICE = new Uint8Array([
+  0xaa, 0xaa, 0x05, 0x00, 0x00, 0x00, 0x02, 0x55, 0x15, 0x00, 0x00, 0x00, 0x00,
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+]);
+
 describe("UsbHidDeviceConnection", () => {
   let device: HIDDevice;
   let apduSender: ApduSenderService;
@@ -30,7 +38,7 @@ describe("UsbHidDeviceConnection", () => {
     expect(cDevice).toStrictEqual(device);
   });
 
-  it("should send APDU through hid report", () => {
+  it("should send APDU through hid report", async () => {
     // given
     const connection = new UsbHidDeviceConnection(
       { device, apduSender, apduReceiver },
@@ -40,5 +48,23 @@ describe("UsbHidDeviceConnection", () => {
     connection.sendApdu(new Uint8Array(0));
     // then
     expect(device.sendReport).toHaveBeenCalled();
+  });
+
+  it("should receive APDU through hid report", async () => {
+    // given
+    device.sendReport = jest.fn(async () => {
+      device.oninputreport!({
+        type: "inputreport",
+        data: new DataView(Uint8Array.from(RESPONSE_LOCKED_DEVICE).buffer),
+      } as HIDInputReportEvent);
+    });
+    const connection = new UsbHidDeviceConnection(
+      { device, apduSender, apduReceiver },
+      logger,
+    );
+    // when
+    const response = connection.sendApdu(Uint8Array.from([]));
+    // then
+    expect(response).resolves.toBe(RESPONSE_LOCKED_DEVICE);
   });
 });
