f5f2ebe59baf7847221bd2be32d41a10fb98c30e
jz_ <jiyustyle2008@gmail.com>
2024-05-23 17:33:23 +0200

    ✨ (core): Implement Close App command

---
+++ packages/core/src/api/command/os/CloseAppCommand.ts
@@ -0,0 +1,34 @@
+import { Apdu } from "@api/apdu/model/Apdu";
+import { ApduBuilder, ApduBuilderArgs } from "@api/apdu/utils/ApduBuilder";
+import { ApduParser } from "@api/apdu/utils/ApduParser";
+import { Command } from "@api/command/Command";
+import { InvalidStatusWordError } from "@api/command/Errors";
+import { CommandUtils } from "@api/command/utils/CommandUtils";
+import { ApduResponse } from "@api/device-session/ApduResponse";
+
+/**
+ * The command to close a runnint application on the device.
+ */
+export class CloseAppCommand implements Command<void> {
+  getApdu(): Apdu {
+    const closeAppApduArgs: ApduBuilderArgs = {
+      cla: 0xb0,
+      ins: 0xa7,
+      p1: 0x00,
+      p2: 0x00,
+    } as const;
+    return new ApduBuilder(closeAppApduArgs).build();
+  }
+
+  parseResponse(apduResponse: ApduResponse): void {
+    const parser = new ApduParser(apduResponse);
+
+    if (!CommandUtils.isSuccessResponse(apduResponse)) {
+      throw new InvalidStatusWordError(
+        `Unexpected status word: ${parser.encodeToHexaString(
+          apduResponse.statusCode,
+        )}`,
+      );
+    }
+  }
+}
