5ad74ee6ad838f724badb2d4cc083c056b31ece4
ofreyssinet-ledger <olivier.freyssinet@ledger.fr>
2024-06-03 20:10:40 +0200

    ⚰️ (poc): Remove unused code in xtate-study

--- pocs/xstate-study/app/page.tsx
+++ pocs/xstate-study/app/page.tsx
@@ -1,16 +1,8 @@
-"use client";
-
-import { useEffect } from "react";
-import Image from "next/image";
-import styles from "./page.module.css";
 import Machine from "./Machine";
 
 export default function Home() {
-  useEffect(() => {
-    console.log("Hello, world!");
-  }, []);
   return (
-    <main className={styles.main}>
+    <main>
       <Machine />
     </main>
   );
