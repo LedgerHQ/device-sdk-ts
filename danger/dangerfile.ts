dad7b2b5569af708ddabf4beff2d6a189328e319
jdabbech-ledger <jalil.dabbech@ledger.com>
2024-07-05 15:00:07 +0200

    :green_heart: (repo): Allow more branch names

--- danger/dangerfile.ts
+++ danger/dangerfile.ts
@@ -1,17 +1,17 @@
 import { danger, fail } from "danger";
 
 const branchRegex =
-  /^(feature|bugfix|support|chore|doc|refacto)\/((dsdk)-[0-9]+|no-issue)\-.*/i;
+  /^(feature|feat|bugfix|bug|hotfix|fix|support|chore|core|doc|refacto|refactor)\/((dsdk)-[0-9]+|no-issue)\-.*/i;
 if (!branchRegex.test(danger.github.pr.head.ref)) {
   fail(
-    "Please fix the PR branch name to match the convention, see [this documentation](https://ledgerhq.atlassian.net/wiki/spaces/WXP/pages/4527358147/DSDK+TS+Git+-+Github+conventions)"
+    "Please fix the PR branch name to match the convention, see [this documentation](https://ledgerhq.atlassian.net/wiki/spaces/WXP/pages/4527358147/DSDK+TS+Git+-+Github+conventions)",
   );
 }
 
 const titleRegex = /^.+ \([a-z]+\) \[(DSDK-[0-9]+|NO-ISSUE)\]: [A-Z].*/;
 if (!titleRegex.test(danger.github.pr.title)) {
   fail(
-    "Please fix the PR title to match the convention, see [this documentation](https://ledgerhq.atlassian.net/wiki/spaces/WXP/pages/4527358147/DSDK+TS+Git+-+Github+conventions)"
+    "Please fix the PR title to match the convention, see [this documentation](https://ledgerhq.atlassian.net/wiki/spaces/WXP/pages/4527358147/DSDK+TS+Git+-+Github+conventions)",
   );
 }
 
@@ -19,7 +19,7 @@ const commitRegex = /^.+ \([a-z]+\): [A-Z].*/;
 for (let { commit } of danger.github.commits) {
   if (!commitRegex.test(commit.message)) {
     fail(
-      "One or more commit message does not match the convention, see [this documentation](https://ledgerhq.atlassian.net/wiki/spaces/WXP/pages/4527358147/DSDK+TS+Git+-+Github+conventions)."
+      "One or more commit message does not match the convention, see [this documentation](https://ledgerhq.atlassian.net/wiki/spaces/WXP/pages/4527358147/DSDK+TS+Git+-+Github+conventions).",
     );
     break;
   }
