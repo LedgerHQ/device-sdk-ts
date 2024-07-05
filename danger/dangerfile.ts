f992712665227d6c066ff72d9e38c09b4657abaa
jdabbech-ledger <jalil.dabbech@ledger.com>
2024-07-05 16:05:58 +0200

    :green_heart: (repo): Allow `-` in comment/title scope

--- danger/dangerfile.ts
+++ danger/dangerfile.ts
@@ -8,14 +8,15 @@ if (!branchRegex.test(danger.github.pr.head.ref)) {
   );
 }
 
-const titleRegex = /^.+ \([a-z]+\) \[(DSDK-[0-9]+|NO-ISSUE)\]: [A-Z].*/;
+const titleRegex =
+  /^.+ \(([a-z]+\-?){1,}\) \[(DSDK-[0-9]+|NO-ISSUE)\]: [A-Z].*/;
 if (!titleRegex.test(danger.github.pr.title)) {
   fail(
     "Please fix the PR title to match the convention, see [this documentation](https://ledgerhq.atlassian.net/wiki/spaces/WXP/pages/4527358147/DSDK+TS+Git+-+Github+conventions)",
   );
 }
 
-const commitRegex = /^.+ \([a-z]+\): [A-Z].*/;
+const commitRegex = /^.+\(([a-z]+\-?){1,}\): [A-Z].*/;
 for (let { commit } of danger.github.commits) {
   if (!commitRegex.test(commit.message)) {
     fail(
