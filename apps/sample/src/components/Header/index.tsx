d42095bd9e7d5d4b68f17edd61322be31dcda0fa
jdabbech-ledger <jalil.dabbech@ledger.com>
2024-07-16 17:00:43 +0200

    :lipstick: (smpl): Mock server url

--- apps/sample/src/components/Header/index.tsx
+++ apps/sample/src/components/Header/index.tsx
@@ -1,10 +1,12 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import {
-  Checkbox,
+  Button,
   DropdownGeneric,
   Flex,
   Icons,
   Input,
+  Switch,
+  Toggle,
 } from "@ledgerhq/react-ui";
 import styled, { DefaultTheme } from "styled-components";
 import { useMockServerContext } from "@/providers/MockServerProvider";
@@ -27,6 +29,10 @@ const IconBox = styled(Flex).attrs({ p: 3 })`
   opacity: 0.7;
 `;
 
+const UrlInput = styled(Input)`
+  align-items: center;
+`;
+
 export const Header = () => {
   const {
     dispatch,
@@ -35,7 +41,16 @@ export const Header = () => {
   const onToggleMockServer = useCallback(() => {
     dispatch({ type: enabled ? "disable_mock_server" : "enable_mock_server" });
   }, [dispatch, enabled]);
+  const [mockServerStateUrl, setMockServerStateUrl] = useState<string>(url);
 
+  const validateServerUrl = useCallback(
+    () =>
+      dispatch({
+        type: "set_mock_server_url",
+        payload: { url: mockServerStateUrl },
+      }),
+    [mockServerStateUrl],
+  );
   return (
     <Root>
       <Actions>
@@ -47,22 +62,25 @@ export const Header = () => {
         </IconBox>
       </Actions>
       <DropdownGeneric closeOnClickOutside label="" placement="bottom">
-        <Flex my={5} py={6} px={5} width={250}>
-          <Checkbox
+        <Flex my={5} py={6} px={5} width={280}>
+          <Switch
             onChange={onToggleMockServer}
-            isChecked={enabled}
+            checked={enabled}
+            name="switch-mock-server"
             label="Enable Mock server"
-            name="mock-server-checkbox"
           />
         </Flex>
         {enabled && (
-          <Input
-            value={url}
-            renderLeft={
-              <Flex alignItems="center" ml={6}>
-                <Icons.Link size="S" />
+          <UrlInput
+            value={mockServerStateUrl}
+            onChange={(url: string) => setMockServerStateUrl(url)}
+            renderRight={() => (
+              <Flex alignItems="center" justifyContent="stretch">
+                <Button iconButton onClick={validateServerUrl}>
+                  <Icons.CheckmarkCircleFill size="S" />
+                </Button>
               </Flex>
-            }
+            )}
           />
         )}
       </DropdownGeneric>
