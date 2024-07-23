0ffc4731697da7a475cfe15894c45d53e1e115be
Louis Aussedat <louis.aussedat@ledger.fr>
2024-07-23 11:49:23 +0200

    ✨ (keyring-eth): Implement SignTransactionCommand

--- packages/signer/keyring-eth/src/internal/app-binder/command/SignTransactionCommand.ts
+++ packages/signer/keyring-eth/src/internal/app-binder/command/SignTransactionCommand.ts
@@ -1,2 +1,139 @@
 // https://github.com/LedgerHQ/app-ethereum/blob/develop/doc/ethapp.adoc#sign-eth-transaction
-export class SignTransactionCommand {}
+import {
+  Apdu,
+  ApduBuilder,
+  ApduBuilderArgs,
+  ApduParser,
+  ApduResponse,
+  type Command,
+  CommandUtils,
+  InvalidStatusWordError,
+} from "@ledgerhq/device-sdk-core";
+import { Just } from "purify-ts";
+import { Nothing } from "purify-ts";
+import { Maybe } from "purify-ts";
+
+const MAX_CHUNK_SIZE = 150;
+const PATH_SIZE = 4;
+const R_LENGTH = 32;
+const S_LENGTH = 32;
+
+export type SignTransactionCommandResponse = Maybe<{
+  v: number;
+  r: `0x${string}`;
+  s: `0x${string}`;
+}>;
+
+export type SignTransactionCommandArgs = {
+  /**
+   * The derivation path to use to sign the transaction.
+   */
+  derivationPath: string;
+
+  /**
+   * The complete serialized transaction data.
+   */
+  data: Uint8Array;
+
+  /**
+   * The index of the chunk to sign.
+   */
+  index: number;
+};
+
+export class SignTransactionCommand
+  implements
+    Command<SignTransactionCommandResponse, SignTransactionCommandArgs>
+{
+  args: SignTransactionCommandArgs;
+
+  constructor(args: SignTransactionCommandArgs) {
+    this.args = args;
+  }
+
+  getApdu(): Apdu {
+    const { data, derivationPath, index } = this.args;
+
+    const signEthTransactionArgs: ApduBuilderArgs = {
+      cla: 0xe0,
+      ins: 0x04,
+      p1: index === 0 ? 0x00 : 0x80,
+      p2: 0x00,
+    };
+    const builder = new ApduBuilder(signEthTransactionArgs);
+    const path = this.splitPath(derivationPath);
+    const dataFirstChunkIndex = MAX_CHUNK_SIZE - path.length * PATH_SIZE - 1;
+
+    if (index === 0) {
+      // add derivation path to the first packet
+      builder.add8BitUintToData(path.length);
+      path.forEach((element) => {
+        builder.add32BitUintToData(element);
+      });
+
+      // add 150 bytes of data minus the path length and the path
+      builder.addBufferToData(data.slice(0, dataFirstChunkIndex));
+    } else {
+      // add 150 bytes of data starting from the second packet
+      builder.addBufferToData(
+        data.slice(
+          dataFirstChunkIndex + (index - 1) * MAX_CHUNK_SIZE,
+          dataFirstChunkIndex + index * MAX_CHUNK_SIZE,
+        ),
+      );
+    }
+
+    return builder.build();
+  }
+
+  parseResponse(response: ApduResponse): SignTransactionCommandResponse {
+    const parser = new ApduParser(response);
+
+    // TODO: handle the error correctly using a generic error handler
+    if (!CommandUtils.isSuccessResponse(response)) {
+      throw new InvalidStatusWordError(
+        `Unexpected status word: ${parser.encodeToHexaString(
+          response.statusCode,
+        )}`,
+      );
+    }
+
+    // The data is returned only for the last chunk
+    const v = parser.extract8BitUint();
+    if (!v) {
+      return Nothing;
+    }
+
+    const r = parser.encodeToHexaString(parser.extractFieldByLength(R_LENGTH));
+    if (!r) {
+      throw new InvalidStatusWordError("R is missing");
+    }
+
+    const s = parser.encodeToHexaString(parser.extractFieldByLength(S_LENGTH));
+    if (!s) {
+      throw new InvalidStatusWordError("S is missing");
+    }
+
+    return Just({
+      v,
+      r: `0x${r}`,
+      s: `0x${s}`,
+    });
+  }
+
+  private splitPath(path: string): number[] {
+    const result: number[] = [];
+    const components = path.split("/");
+    components.forEach((element) => {
+      let number = parseInt(element, 10);
+      if (isNaN(number)) {
+        return; // FIXME: shouldn't it throws instead?
+      }
+      if (element.length > 1 && element[element.length - 1] === "'") {
+        number += 0x80000000;
+      }
+      result.push(number);
+    });
+    return result;
+  }
+}
