842aa825ce41faa4b45b97ccd701f376ad604f2f
jdabbech-ledger <jalil.dabbech@ledger.com>
2024-07-26 11:35:38 +0200

    :rotating_light: (transport-mock): Fix lint issues

--- packages/transport-mock/src/model/Mock.ts
+++ packages/transport-mock/src/model/Mock.ts
@@ -2,10 +2,7 @@ export class Mock {
   prefix: string;
   response: string;
 
-  constructor(
-    prefix: string,
-    response: string,
-  ) {
+  constructor(prefix: string, response: string) {
     this.prefix = prefix;
     this.response = response;
   }
