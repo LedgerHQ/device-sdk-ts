aef28e6041adfb805e0cf1b5aa812305452b0892
Louis Aussedat <louis.aussedat@ledger.fr>
2024-10-07 15:28:27 +0200

    ✨ (sample): Add support of calUrl and testing endpoint

--- apps/sample/src/components/KeyringEthView/index.tsx
+++ apps/sample/src/components/KeyringEthView/index.tsx
@@ -20,12 +20,31 @@ import {
 import { DeviceActionProps } from "@/components/DeviceActionsView/DeviceActionTester";
 import { ethers } from "ethers";
 import { DeviceActionsList } from "@/components/DeviceActionsView/DeviceActionsList";
+import { ContextModuleBuilder } from "@ledgerhq/context-module";
 
 export const KeyringEthView: React.FC<{ sessionId: string }> = ({
   sessionId,
 }) => {
   const sdk = useSdk();
-  const keyring = new KeyringEthBuilder({ sdk, sessionId }).build();
+
+  const getKeyringEth = (test: boolean = false, calUrl?: string) => {
+    const builder = new ContextModuleBuilder();
+    if (test) {
+      builder.withConfig({
+        cal: {
+          url:
+            calUrl && calUrl.length
+              ? calUrl
+              : "https://crypto-assets-service.api.ledger-test.com/v1",
+          mode: "test",
+        },
+      });
+    }
+    const contextModule = builder.build();
+    return new KeyringEthBuilder({ sdk, sessionId })
+      .withContextModule(contextModule)
+      .build();
+  };
 
   const deviceModelId = sdk.getConnectedDevice({
     sessionId,
@@ -43,7 +62,7 @@ export const KeyringEthView: React.FC<{ sessionId: string }> = ({
           checkOnDevice,
           returnChainCode,
         }) => {
-          return keyring.getAddress(derivationPath, {
+          return getKeyringEth().getAddress(derivationPath, {
             checkOnDevice,
             returnChainCode,
           });
@@ -69,7 +88,7 @@ export const KeyringEthView: React.FC<{ sessionId: string }> = ({
         description:
           "Perform all the actions necessary to sign a message with the device",
         executeDeviceAction: ({ derivationPath, message }) => {
-          return keyring.signMessage(derivationPath, message);
+          return getKeyringEth().signMessage(derivationPath, message);
         },
         initialValues: {
           derivationPath: "44'/60'/0'/0/0",
@@ -94,7 +113,7 @@ export const KeyringEthView: React.FC<{ sessionId: string }> = ({
           transaction,
           recipientDomain,
         }) => {
-          return keyring.signTransaction(
+          return getKeyringEth().signTransaction(
             derivationPath,
             ethers.Transaction.from(transaction),
             { domain: recipientDomain },
@@ -120,13 +139,18 @@ export const KeyringEthView: React.FC<{ sessionId: string }> = ({
         title: "Sign typed message",
         description:
           "Perform all the actions necessary to sign a typed message on the device",
-        executeDeviceAction: ({ derivationPath, message }) => {
+        executeDeviceAction: ({ derivationPath, message, test, calUrl }) => {
           const typedData = JSON.parse(message) as TypedData;
-          return keyring.signTypedData(derivationPath, typedData);
+          return getKeyringEth(test, calUrl).signTypedData(
+            derivationPath,
+            typedData,
+          );
         },
         initialValues: {
           derivationPath: "44'/60'/0'/0/0",
           message: `{"domain":{"name":"USD Coin","verifyingContract":"0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48","chainId":1,"version":"2"},"primaryType":"Permit","message":{"deadline":1718992051,"nonce":0,"spender":"0x111111125421ca6dc452d289314280a0f8842a65","owner":"0x6cbcd73cd8e8a42844662f0a0e76d7f79afd933d","value":"115792089237316195423570985008687907853269984665640564039457584007913129639935"},"types":{"EIP712Domain":[{"name":"name","type":"string"},{"name":"version","type":"string"},{"name":"chainId","type":"uint256"},{"name":"verifyingContract","type":"address"}],"Permit":[{"name":"owner","type":"address"},{"name":"spender","type":"address"},{"name":"value","type":"uint256"},{"name":"nonce","type":"uint256"},{"name":"deadline","type":"uint256"}]}}`,
+          test: false,
+          calUrl: "",
         },
         validateValues: ({ message }) => {
           try {
@@ -155,6 +179,8 @@ export const KeyringEthView: React.FC<{ sessionId: string }> = ({
         {
           derivationPath: string;
           message: string;
+          test: boolean;
+          calUrl: string;
         },
         SignTypedDataDAError,
         SignTypedDataDAIntermediateValue
