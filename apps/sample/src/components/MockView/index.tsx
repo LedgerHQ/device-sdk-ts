9158854c0ad8b9f34e98eeefd9bc331ad27b5866
jdabbech-ledger <jalil.dabbech@ledger.com>
2024-10-23 17:24:12 +0200

    :lipstick: (smpl): Review mocksettings edit mock

--- apps/sample/src/components/MockView/index.tsx
+++ apps/sample/src/components/MockView/index.tsx
@@ -1,29 +1,25 @@
 import React, { useCallback, useEffect, useState } from "react";
-import { Mock, Session } from "@ledgerhq/device-sdk-transport-mock";
-import { Button, Divider, Flex, Input, Text } from "@ledgerhq/react-ui";
+import { Session } from "@ledgerhq/device-sdk-transport-mock";
+import { Button, Flex, Text } from "@ledgerhq/react-ui";
 import styled from "styled-components";
 
 import { ClickableListItem } from "@/components/ClickableListItem";
+import { MockDeviceDrawer } from "@/components/MockView/MockDeviceDrawer";
 import { PageWithHeader } from "@/components/PageWithHeader";
-import { StyledDrawer } from "@/components/StyledDrawer";
 import { useMockClient } from "@/hooks/useMockClient";
 import { useSdkConfigContext } from "@/providers/SdkConfig";
 
 const MockButton = styled(Button).attrs({
   variant: "main",
-  size: "small",
   color: "neutral.c00",
   mx: 5,
 })``;
 
 export const MockView: React.FC = () => {
   const [sessions, setSessions] = useState<Session[]>([]);
-  const [mocks, setMocks] = useState<Mock[]>([]);
 
   const [currentSession, setCurrentSession] = useState<Session | null>(null);
   const [drawerVisible, setDrawerVisibility] = useState<boolean>(false);
-  const [currentPrefix, setCurrentPrefix] = useState<string>("b001");
-  const [currentResponse, setCurrentResponse] = useState<string>("6700");
 
   const {
     state: { mockServerUrl },
@@ -41,83 +37,14 @@ export const MockView: React.FC = () => {
     }
   }, [client]);
 
-  const fetchMocks = useCallback(
-    async (session: Session) => {
-      try {
-        const response = await client.getMocks(session.id);
-        setMocks(response);
-      } catch (error) {
-        console.error(error);
-      }
-    },
-    [client],
-  );
-
-  const handleSessionClick = useCallback(
-    async (session: Session) => {
-      try {
-        setCurrentSession(session);
-        fetchMocks(session)
-          .catch(console.error)
-          .finally(() => setDrawerVisibility(true));
-      } catch (error) {
-        console.error(error);
-      }
-    },
-    [fetchMocks],
-  );
-
-  const handleAddMockClick = useCallback(async () => {
-    if (!currentSession) {
-      return;
-    }
+  const handleSessionClick = useCallback(async (session: Session) => {
     try {
-      const response = await client.addMock(
-        currentSession.id,
-        currentPrefix,
-        currentResponse,
-      );
-      if (!response) {
-        console.log("Failed to add the mock");
-      } else {
-        fetchMocks(currentSession).catch(console.error);
-      }
+      setCurrentSession(session);
+      setDrawerVisibility(true);
     } catch (error) {
       console.error(error);
     }
-  }, [client, currentPrefix, currentResponse, currentSession, fetchMocks]);
-
-  const handleRemoveMocksClick = async () => {
-    if (!currentSession) {
-      return;
-    }
-    try {
-      const response = await client.deleteMocks(currentSession.id);
-      if (!response) {
-        console.log("Failed to delete the mocks");
-      } else {
-        fetchMocks(currentSession).catch(console.error);
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const handleRemoveDevice = useCallback(
-    async (sessionId: string) => {
-      try {
-        const response = await client.disconnect(sessionId);
-        if (!response) {
-          console.log("Failed to disconnect device");
-        } else {
-          fetchSessions().catch(console.error);
-        }
-      } catch (error) {
-        console.error(error);
-      }
-    },
-    [client, fetchSessions],
-  );
+  }, []);
 
   const handleRemoveDevicesClick = async () => {
     try {
@@ -136,8 +63,6 @@ export const MockView: React.FC = () => {
     fetchSessions().catch(console.error);
   }, [fetchSessions]);
 
-  const inputContainerProps = { style: { borderRadius: 4 } };
-
   return (
     <PageWithHeader title="Mock server">
       <Flex flexDirection="column" flex={2} justifyContent="space-between">
@@ -153,104 +78,19 @@ export const MockView: React.FC = () => {
           ))}
         </div>
         <Flex alignSelf="flex-end">
-          <MockButton onClick={() => handleRemoveDevicesClick()}>
+          <MockButton onClick={handleRemoveDevicesClick}>
             <Text color="neutral.c00">Remove all devices</Text>
           </MockButton>
         </Flex>
       </Flex>
 
-      <StyledDrawer
+      <MockDeviceDrawer
         isOpen={drawerVisible}
         onClose={() => setDrawerVisibility(false)}
-        big
-        title={currentSession?.device?.name || "No device"}
-      >
-        {currentSession && (
-          <Flex flexDirection="column" flex={2} justifyContent="space-between">
-            <div>
-              <Flex
-                flexDirection="row"
-                flex={5}
-                alignItems="center"
-                justifyContent="center"
-              >
-                <Text style={{ flex: 2 }} textAlign="center" variant="h5">
-                  Prefix
-                </Text>
-                <Text style={{ flex: 2 }} textAlign="center" variant="h5">
-                  Response
-                </Text>
-                <Flex style={{ flex: 1 }} />
-              </Flex>
-              <Divider my={5} />
-              <div style={{ overflow: "scroll", maxHeight: "400px" }}>
-                {mocks.map((mock, index) => (
-                  <Flex
-                    flexDirection="row"
-                    key={`${index}`}
-                    flex={5}
-                    alignItems="center"
-                    justifyContent="center"
-                    my={7}
-                  >
-                    <Text variant="body" style={{ flex: 2 }} textAlign="center">
-                      {mock.prefix}
-                    </Text>
-                    <Text variant="body" style={{ flex: 2 }} textAlign="center">
-                      {mock.response}
-                    </Text>
-                    <Flex style={{ flex: 1 }} />
-                  </Flex>
-                ))}
-              </div>
-
-              <Flex flexDirection="row" justifyContent="space-between" flex={5}>
-                <Flex
-                  flexDirection="column"
-                  flex={2}
-                  justifyContent="center"
-                  alignItems="center"
-                >
-                  <Input
-                    style={{ width: "32px" }}
-                    name="APDU Prefix"
-                    containerProps={inputContainerProps}
-                    value={currentPrefix}
-                    onChange={(value) => setCurrentPrefix(value)}
-                  />
-                </Flex>
-                <Flex
-                  flexDirection="column"
-                  flex={2}
-                  justifyContent="center"
-                  alignItems="center"
-                >
-                  <Input
-                    style={{ width: "32px" }}
-                    name="Mock response"
-                    containerProps={inputContainerProps}
-                    value={currentResponse}
-                    onChange={(value) => setCurrentResponse(value)}
-                  />
-                </Flex>
-                <Flex flex={1} alignItems="center">
-                  <MockButton onClick={() => handleAddMockClick()}>
-                    <Text color="neutral.c00">Add</Text>
-                  </MockButton>
-                </Flex>
-              </Flex>
-            </div>
-            <Flex alignSelf="flex-end">
-              <MockButton onClick={handleRemoveMocksClick}>
-                <Text color="neutral.c00">Remove mocks</Text>
-              </MockButton>
-              <MockButton onClick={() => handleRemoveDevice(currentSession.id)}>
-                <Text color="neutral.c00">Remove device</Text>
-              </MockButton>
-            </Flex>
-          </Flex>
-        )}
-      </StyledDrawer>
+        onDeviceDeleted={fetchSessions}
+        currentSession={currentSession}
+        client={client}
+      />
     </PageWithHeader>
   );
 };
