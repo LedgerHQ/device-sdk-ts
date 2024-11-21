25eb39d1f6a8a098599512e266f8cd6553ba62cd
fAnselmi-Ledger <francesco.anselmi@ledger.fr>
2024-11-21 17:52:47 +0100

    ✨ (signer-btc): Implement clientCommands

---
+++ packages/signer/signer-btc/src/internal/app-binder/command/client-command-handlers/GetMerkleLeafIndexCommandHandler.test.ts
@@ -0,0 +1,180 @@
+import { Just, Nothing } from "purify-ts";
+
+import {
+  BUFFER_SIZE,
+  ClientCommandCodes,
+} from "@internal/app-binder/command/utils/constants";
+import { type DataStore } from "@internal/data-store/model/DataStore";
+import { encodeVarint } from "@internal/utils/Varint";
+
+import { type CommandHandlerContext } from "./ClientCommandHandlersTypes";
+import { GetMerkleLeafIndexCommandHandler } from "./GetMerkleLeafIndexCommandHandler";
+
+const COMMAND_CODE = ClientCommandCodes.GET_MERKLE_LEAF_INDEX;
+
+describe("GetMerkleLeafIndexCommandHandler", () => {
+  let commandHandlerContext: CommandHandlerContext;
+  let mockDataStore: jest.Mocked<DataStore>;
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+
+    mockDataStore = {
+      getMerkleLeafIndex: jest.fn(),
+    } as unknown as jest.Mocked<DataStore>;
+
+    commandHandlerContext = {
+      dataStore: mockDataStore,
+      queue: [],
+      yieldedResults: [],
+    };
+  });
+
+  const createRequest = (
+    commandCode: number,
+    merkleRootHash: Uint8Array,
+    leafNodeHash: Uint8Array,
+  ): Uint8Array =>
+    new Uint8Array([commandCode, ...merkleRootHash, ...leafNodeHash]);
+
+  it("should return the index when the Merkle leaf is found", () => {
+    // given
+    const merkleRootHash = new Uint8Array(BUFFER_SIZE).fill(0xaa);
+    const leafNodeHash = new Uint8Array(BUFFER_SIZE).fill(0xff);
+    const handlerRequest = createRequest(
+      COMMAND_CODE,
+      merkleRootHash,
+      leafNodeHash,
+    );
+    const leafIndex = 123456;
+    const encodedLeafIndexVarint = encodeVarint(leafIndex).unsafeCoerce();
+
+    // when
+    mockDataStore.getMerkleLeafIndex.mockReturnValue(Just(leafIndex));
+
+    const handlerResponse = GetMerkleLeafIndexCommandHandler(
+      handlerRequest,
+      commandHandlerContext,
+    );
+
+    const expectedResponseBuffer = new Uint8Array(
+      1 + encodedLeafIndexVarint.length,
+    );
+    expectedResponseBuffer[0] = 1; // index found
+    expectedResponseBuffer.set(encodedLeafIndexVarint, 1);
+
+    // then
+    expect(mockDataStore.getMerkleLeafIndex).toHaveBeenCalledWith(
+      merkleRootHash,
+      leafNodeHash,
+    );
+    expect(handlerResponse.isRight()).toBe(true);
+    expect(handlerResponse.unsafeCoerce()).toEqual(expectedResponseBuffer);
+    expect(commandHandlerContext.queue).toHaveLength(0);
+  });
+
+  it("should return a failure response when the Merkle leaf is not found", () => {
+    // given
+    const merkleRootHash = new Uint8Array(BUFFER_SIZE).fill(0xaa);
+    const leafNodeHash = new Uint8Array(BUFFER_SIZE).fill(0xff);
+    const handlerRequest = createRequest(
+      COMMAND_CODE,
+      merkleRootHash,
+      leafNodeHash,
+    );
+
+    // when
+    mockDataStore.getMerkleLeafIndex.mockReturnValue(Nothing);
+
+    const handlerResponse = GetMerkleLeafIndexCommandHandler(
+      handlerRequest,
+      commandHandlerContext,
+    );
+
+    const expectedFailureResponseBuffer = Uint8Array.from([0]);
+
+    // then
+    expect(mockDataStore.getMerkleLeafIndex).toHaveBeenCalledWith(
+      merkleRootHash,
+      leafNodeHash,
+    );
+    expect(handlerResponse.isRight()).toBe(true);
+    expect(handlerResponse.unsafeCoerce()).toEqual(
+      expectedFailureResponseBuffer,
+    );
+    expect(commandHandlerContext.queue).toHaveLength(0);
+  });
+
+  it("should correctly handle a leaf index of zero", () => {
+    // given
+    const merkleRootHash = new Uint8Array(BUFFER_SIZE).fill(0xaa);
+    const leafNodeHash = new Uint8Array(BUFFER_SIZE).fill(0xff);
+    const handlerRequest = createRequest(
+      COMMAND_CODE,
+      merkleRootHash,
+      leafNodeHash,
+    );
+    const leafIndex = 0;
+    const encodedLeafIndexVarint = encodeVarint(leafIndex).unsafeCoerce();
+
+    // when
+    mockDataStore.getMerkleLeafIndex.mockReturnValue(Just(leafIndex));
+
+    const handlerResponse = GetMerkleLeafIndexCommandHandler(
+      handlerRequest,
+      commandHandlerContext,
+    );
+
+    const expectedResponseBuffer = new Uint8Array(
+      1 + encodedLeafIndexVarint.length,
+    );
+    expectedResponseBuffer[0] = 1; // index found
+    expectedResponseBuffer.set(encodedLeafIndexVarint, 1);
+
+    // then
+    expect(mockDataStore.getMerkleLeafIndex).toHaveBeenCalledWith(
+      merkleRootHash,
+      leafNodeHash,
+    );
+    expect(handlerResponse.isRight()).toBe(true);
+    expect(handlerResponse.unsafeCoerce()).toEqual(expectedResponseBuffer);
+    expect(commandHandlerContext.queue).toHaveLength(0);
+  });
+
+  it("should correctly handle the maximum BUFFER_SIZE-bit unsigned integer as a leaf index", () => {
+    // given
+    const merkleRootHash = new Uint8Array(BUFFER_SIZE).fill(0xaa);
+    const leafNodeHash = new Uint8Array(BUFFER_SIZE).fill(0xff);
+    const handlerRequest = createRequest(
+      COMMAND_CODE,
+      merkleRootHash,
+      leafNodeHash,
+    );
+    const maximumLeafIndex = 0xffffffff; // maximum value of a BUFFER_SIZE-bit unsigned integer
+    const encodedLeafIndexVarint =
+      encodeVarint(maximumLeafIndex).unsafeCoerce();
+
+    // when
+    mockDataStore.getMerkleLeafIndex.mockReturnValue(Just(maximumLeafIndex));
+
+    const handlerResponse = GetMerkleLeafIndexCommandHandler(
+      handlerRequest,
+      commandHandlerContext,
+    );
+
+    const expectedResponseBuffer = new Uint8Array(
+      1 + encodedLeafIndexVarint.length,
+    );
+    expectedResponseBuffer[0] = 1; // index found
+    expectedResponseBuffer.set(encodedLeafIndexVarint, 1);
+
+    // then
+    expect(mockDataStore.getMerkleLeafIndex).toHaveBeenCalledWith(
+      merkleRootHash,
+      leafNodeHash,
+    );
+    expect(handlerResponse.isRight()).toBe(true);
+    expect(handlerResponse.unsafeCoerce()).toEqual(expectedResponseBuffer);
+    expect(commandHandlerContext.queue).toHaveLength(0);
+  });
+});
--- packages/signer/signer-btc/src/internal/app-binder/command/utils/constants.ts
+++ packages/signer/signer-btc/src/internal/app-binder/command/utils/constants.ts
@@ -1,1 +1,11 @@
 export const PROTOCOL_VERSION = 1;
+
+export const BUFFER_SIZE = 32;
+
+export enum ClientCommandCodes {
+  YIELD = 0x10,
+  GET_PREIMAGE = 0x40,
+  GET_MERKLE_LEAF_PROOF = 0x41,
+  GET_MERKLE_LEAF_INDEX = 0x42,
+  GET_MORE_ELEMENTS = 0xa0,
+}
