c380df6140bea4768aa5b088ceb1279bc31439b0
Mathieu Bertin <mathieu.bertin@ledger.fr>
2024-12-04 17:42:17 +0100

    ⬆️  (doc): Migrate to Nextra v3

--- apps/docs/next.config.js
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const withNextra = require("nextra")({
-  defaultShowCopyCode: true,
-  theme: "nextra-theme-docs",
-  themeConfig: "./theme.config.tsx",
-});
-
-module.exports = withNextra();
