ee368fd4e4e8af1ebf0af2d7a3fe687618f9a831
jdabbech-ledger <jalil.dabbech@ledger.com>
2025-01-20 15:00:54 +0100

    ✨ (sample): Add transport ble in react native sample app

---
+++ apps/mobile/src/hooks/useDeviceSessionState.ts
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import {
+  DeviceSessionId,
+  DeviceSessionState,
+  DeviceStatus,
+} from "@ledgerhq/device-management-kit";
+
+import { useSdk } from "@/providers/DeviceSdkProvider";
+import { useDeviceSessionsContext } from "@/providers/DeviceSessionsProvider";
+
+export function useDeviceSessionState(sessionId: DeviceSessionId) {
+  const sdk = useSdk();
+  const [deviceSessionState, setDeviceSessionState] =
+    useState<DeviceSessionState>();
+  const { dispatch } = useDeviceSessionsContext();
+
+  useEffect(() => {
+    if (sessionId) {
+      const subscription = sdk
+        .getDeviceSessionState({
+          sessionId,
+        })
+        .subscribe((state) => {
+          if (state.deviceStatus === DeviceStatus.NOT_CONNECTED) {
+            dispatch({ type: "remove_session", payload: { sessionId } });
+          } else {
+            setDeviceSessionState(state);
+          }
+        });
+
+      return () => {
+        subscription.unsubscribe();
+      };
+    }
+  }, [sessionId, sdk, dispatch]);
+
+  return deviceSessionState;
+}
---
+++ apps/mobile/src/navigators/RootNavigator.constants.ts
@@ -0,0 +1,8 @@
+export const Screens = {
+  ROOT_SCREEN: 'ROOT_SCREEN',
+  COMMANDS_SCREEN: 'COMMANDS_SCREEN',
+};
+
+export const Navigators = {
+  ROOT: 'ROOT_NAVIGATOR',
+};
