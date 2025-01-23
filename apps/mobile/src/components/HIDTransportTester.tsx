417b245d2f7896eccbff04c3ec2d94b7764f38e1
Olivier Freyssinet <olivier.freyssinet@ledger.fr>
2025-01-23 17:12:07 +0100

    wip basic reactmethod test

--- apps/mobile/src/components/HIDTransportTester.tsx
+++ apps/mobile/src/components/HIDTransportTester.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useEffect} from 'react';
+import {View, Text, NativeModules, Alert} from 'react-native';
+
+const TransportModule = NativeModules.RCTTransportHIDModule;
 
 export function HIDTransportTester() {
+  useEffect(() => {
+    TransportModule.test().then(res => Alert.alert('yo', res));
+  });
   return (
     <View>
       <Text>yo</Text>
--- packages/transport/rn-hid/android/src/main/kotlin/com/ledger/androidtransporthid/TransportHidModule.kt
+++ packages/transport/rn-hid/android/src/main/kotlin/com/ledger/androidtransporthid/TransportHidModule.kt
@@ -1,7 +1,14 @@
 package com.ledger.androidtransporthid
+import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
+import com.facebook.react.bridge.ReactMethod
 
 class TransportHidModule(reactContext: ReactApplicationContext): ReactContextBaseJavaModule(reactContext) {
     override fun getName(): String = "RCTTransportHIDModule"
+
+    @ReactMethod
+    fun test(promise: Promise) {
+        promise.resolve("test OK")
+    }
 }
