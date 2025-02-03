fac45fbe291b4bed93f1c21f6cc8a96f598d6033
Valentin D. Pinkman <valentin.dealmeida@ledger.fr>
2025-02-03 17:54:48 +0100

    ✨ (packages): Vitest setup to trusted-apps and ui packages

--- packages/trusted-apps/jest.config.ts
+++
@@ -1,8 +0,0 @@
-/* eslint no-restricted-syntax: 0 */
-import type { JestConfigWithTsJest } from "ts-jest";
-
-const config: JestConfigWithTsJest = {
-  preset: "@ledgerhq/jest-config-dsdk",
-};
-
-export default config;
