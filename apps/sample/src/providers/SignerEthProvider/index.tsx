b559a45ecacbda4929f61a410edcab88d1961812
Louis Aussedat <louis.aussedat@ledger.fr>
2025-02-03 10:31:43 +0100

    ✨ (sample): Add web3checks config

--- apps/sample/src/providers/SignerEthProvider/index.tsx
+++ apps/sample/src/providers/SignerEthProvider/index.tsx
@@ -10,6 +10,7 @@ import React, {
 import {
   ContextModuleBuilder,
   type ContextModuleCalConfig,
+  type ContextModuleWeb3ChecksConfig,
 } from "@ledgerhq/context-module";
 import {
   type SignerEth,
@@ -22,7 +23,9 @@ import { useDeviceSessionsContext } from "@/providers/DeviceSessionsProvider";
 type SignerEthContextType = {
   signer: SignerEth | null;
   calConfig: ContextModuleCalConfig;
+  web3ChecksConfig: ContextModuleWeb3ChecksConfig;
   setCalConfig: (cal: ContextModuleCalConfig) => void;
+  setWeb3ChecksConfig: (web3Checks: ContextModuleWeb3ChecksConfig) => void;
 };
 
 const initialState: SignerEthContextType = {
@@ -32,7 +35,11 @@ const initialState: SignerEthContextType = {
     mode: "prod",
     branch: "main",
   },
+  web3ChecksConfig: {
+    url: "https://web3checks-backend.api.aws.prd.ldg-tech.com/v3",
+  },
   setCalConfig: () => {},
+  setWeb3ChecksConfig: () => {},
 };
 
 const SignerEthContext = createContext<SignerEthContextType>(initialState);
@@ -49,6 +56,8 @@ export const SignerEthProvider: React.FC<PropsWithChildren> = ({
   const [calConfig, setCalConfig] = useState<ContextModuleCalConfig>(
     initialState.calConfig,
   );
+  const [web3ChecksConfig, setWeb3ChecksConfig] =
+    useState<ContextModuleWeb3ChecksConfig>(initialState.web3ChecksConfig);
 
   useEffect(() => {
     if (!sessionId || !dmk) {
@@ -58,15 +67,24 @@ export const SignerEthProvider: React.FC<PropsWithChildren> = ({
 
     const contextModule = new ContextModuleBuilder()
       .addCalConfig(calConfig)
+      .addWeb3ChecksConfig(web3ChecksConfig)
       .build();
     const newSigner = new SignerEthBuilder({ dmk, sessionId })
       .withContextModule(contextModule)
       .build();
     setSigner(newSigner);
-  }, [calConfig, dmk, sessionId]);
+  }, [calConfig, dmk, sessionId, web3ChecksConfig]);
 
   return (
-    <SignerEthContext.Provider value={{ signer, calConfig, setCalConfig }}>
+    <SignerEthContext.Provider
+      value={{
+        signer,
+        calConfig,
+        setCalConfig,
+        web3ChecksConfig,
+        setWeb3ChecksConfig,
+      }}
+    >
       {children}
     </SignerEthContext.Provider>
   );
@@ -79,4 +97,10 @@ export const useSignerEth = (): SignerEth | null => {
 export const useCalConfig = () => {
   const { calConfig, setCalConfig } = useContext(SignerEthContext);
   return { calConfig, setCalConfig };
+};
+
+export const useWeb3ChecksConfig = () => {
+  const { web3ChecksConfig, setWeb3ChecksConfig } =
+    useContext(SignerEthContext);
+  return { web3ChecksConfig, setWeb3ChecksConfig };
 };
