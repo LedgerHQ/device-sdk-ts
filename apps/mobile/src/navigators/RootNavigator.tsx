a6ff1d4fcc366d75b9f79bac09a85edfe8d81fac
Valentin D. Pinkman <valentin.dealmeida@ledger.fr>
2025-02-04 10:34:49 +0100

    :bento: (mobile): Add assets

--- apps/mobile/src/navigators/RootNavigator.tsx
+++ apps/mobile/src/navigators/RootNavigator.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Screens } from "./RootNavigator.constants";
 import { HomeScreen } from "_components/HomeScreen";
+import { CommandsScreen } from "_components/CommandsScreen";
 import { createStackNavigator } from "@react-navigation/stack";
-
 const RootStackNavigator = createStackNavigator();
 
 export const RootNavigator = () => (
@@ -10,6 +10,11 @@ export const RootNavigator = () => (
     <RootStackNavigator.Screen
       name={Screens.ROOT_SCREEN}
       component={HomeScreen}
+      options={{ header: () => null }}
+    />
+    <RootStackNavigator.Screen
+      name={Screens.COMMANDS_SCREEN}
+      component={CommandsScreen}
       options={{ header: () => null }}
     />
   </RootStackNavigator.Navigator>
