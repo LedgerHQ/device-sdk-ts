cce8005a908bc3d9c4d40a552d43a66b72aa5b29
jiyuzhuang <jiyu.zhuang@ledger.fr>
2025-02-05 14:24:31 +0100

    ✨ (dmk): Add secure channel connection task

---
+++ packages/device-management-kit/src/api/secure-channel/utils.ts
@@ -0,0 +1,29 @@
+/**
+ * Checks if the given APDU command will request permission.
+ *
+ * This method checks if the first byte of the APDU command is `0xe0` and the second byte is `0x51`.
+ *
+ * @param apdu - The APDU command as a Uint8Array.
+ * @returns `true` if the APDU command will request permission, otherwise `false`.
+ */
+export function willRequestPermission(apdu: Uint8Array): boolean {
+  return apdu[0] === 0xe0 && apdu[1] === 0x51;
+}
+
+/**
+ * Checks if the user refused the permission on the device.
+ *
+ * The following status codes indicate a refusal:
+ * - "0x5501": User refused the permission.
+ * - "0x6985": Condition of use not satisfied.
+ *
+ * @param statusCode - The status code to check.
+ * @returns `true` if the status code indicates a refusal, otherwise `false`.
+ */
+export function isRefusedByUser(statusCode: Uint8Array): boolean {
+  return (
+    statusCode.length === 2 &&
+    ((statusCode[0] === 0x55 && statusCode[1] === 0x01) ||
+      (statusCode[0] === 0x69 && statusCode[1] === 0x85))
+  );
+}
