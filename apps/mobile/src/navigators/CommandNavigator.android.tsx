34c5d9990b7d5ee1aa5a47188c76bef8e812cb63
jdabbech-ledger <jalil.dabbech@ledger.com>
2025-02-07 17:53:55 +0100

    💄 (mobile): UI improvements

--- apps/mobile/src/navigators/CommandNavigator.android.tsx
+++ apps/mobile/src/navigators/CommandNavigator.android.tsx
@@ -1,25 +1,21 @@
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import { CommandsScreens } from "_navigators/CommandNavigator.constants.ts";
 import { CommandTesterScreen } from "_components/CommandTesterScreen.tsx";
-import theme from "@ledgerhq/native-ui/styles/theme";
 import { DeviceActionTesterScreen } from "_components/DeviceActionTesterScreen.tsx";
 
 export const CommandNavigator = createMaterialTopTabNavigator({
   screens: {
-    [CommandsScreens.CommandTester]: {
-      screen: CommandTesterScreen,
-      options: {
-        tabBarLabel: "Command tester",
-      },
-    },
     [CommandsScreens.DeviceActionTester]: {
       screen: DeviceActionTesterScreen,
       options: {
-        tabBarLabel: "Device action tester",
+        tabBarLabel: "Device actions",
       },
     },
-  },
-  screenOptions: {
-    tabBarStyle: { backgroundColor: theme.colors.background.main },
+    [CommandsScreens.CommandTester]: {
+      screen: CommandTesterScreen,
+      options: {
+        tabBarLabel: "Commands",
+      },
+    },
   },
 });
