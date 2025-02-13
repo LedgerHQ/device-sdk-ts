207966b5c92502b9a29eb92635ff8903bd8ade77
ofreyssinet-ledger <olivier.freyssinet@ledger.fr>
2025-02-13 11:25:53 +0100

    ♻️ (rn-hid): Cleanup Kotlin code

--- packages/transport/rn-hid/android/src/main/kotlin/com/ledger/androidtransporthid/bridge/serialization.kt
+++ packages/transport/rn-hid/android/src/main/kotlin/com/ledger/androidtransporthid/bridge/serialization.kt
@@ -15,114 +15,109 @@ import com.ledger.devicesdk.shared.internal.service.logger.LogLevel
 import com.ledger.devicesdk.shared.internal.transport.TransportEvent
 import kotlinx.datetime.Clock
 
-fun LedgerDevice.toWritableMap(): WritableMap {
-    val map = Arguments.createMap()
-    map.putString("name", this.name)
-    map.putString("usbProductIdMask", this.usbInfo.productIdMask)
-    return map
-}
+fun LedgerDevice.toWritableMap(): WritableMap =
+    Arguments.createMap().apply {
+        putString("name", name)
+        putString("usbProductIdMask", usbInfo.productIdMask)
+    }
 
-fun DiscoveryDevice.toWritableMap(): WritableMap {
-    val map = Arguments.createMap()
-    map.putString("uid", this.uid)
-    map.putString("name", this.name)
-    map.putMap("ledgerDevice", this.ledgerDevice.toWritableMap())
-    return map
-}
+fun DiscoveryDevice.toWritableMap(): WritableMap =
+    Arguments.createMap().apply {
+        putString("uid", uid)
+        putString("name", name)
+        putMap("ledgerDevice", ledgerDevice.toWritableMap())
+    }
 
-internal fun LogLevel.toSerializedString(): String {
-    return when (this) {
+
+internal fun LogLevel.toSerializedString(): String =
+    when (this) {
         LogLevel.DEBUG -> "debug"
         LogLevel.INFO -> "info"
         LogLevel.WARNING -> "warning"
         LogLevel.ERROR -> "error"
     }
-}
 
-internal fun LogInfo.toWritableMap(): WritableMap {
-    val map = Arguments.createMap()
-    map.putString("level", this.level.toSerializedString())
-    map.putString("tag", "[TransportHidModule][${this.tag}]")
-    map.putString("message", this.message)
-    map.putMap("jsonPayLoad", Arguments.makeNativeMap(this.jsonPayLoad))
-    map.putString("timestamp", Clock.System.now().toEpochMilliseconds().toString())
-    return map
-}
 
-internal fun InternalConnectionResult.toWritableMap(): WritableMap {
+internal fun LogInfo.toWritableMap(): WritableMap =
+    Arguments.createMap().apply {
+        putString("level", level.toSerializedString())
+        putString("tag", "[TransportHidModule][${tag}]")
+        putString("message", message)
+        putMap("jsonPayLoad", Arguments.makeNativeMap(jsonPayLoad))
+        putString("timestamp", Clock.System.now().toEpochMilliseconds().toString())
+    }
+
+internal fun InternalConnectionResult.toWritableMap(): WritableMap =
     when (this) {
         is InternalConnectionResult.Connected -> {
-            val map = Arguments.createMap()
-            map.putBoolean("success", true)
-            map.putString("sessionId", this.sessionId)
-            map.putMap("ledgerDevice", this.device.ledgerDevice.toWritableMap())
-            map.putString("deviceName", this.device.name)
-            return map
+            Arguments.createMap().apply {
+                putBoolean("success", true)
+                putString("sessionId", sessionId)
+                putMap("ledgerDevice", device.ledgerDevice.toWritableMap())
+                putString("deviceName", device.name)
+            }
         }
         is InternalConnectionResult.ConnectionError -> {
-            val map = Arguments.createMap()
-            map.putBoolean("success", false)
-            map.putString("error", when (this.error) {
-                /**
-                 * Most of these errors are for the BLE lib so it does not really make sense
-                 * to have them here but for the sake of being explicit and not using an "else"
-                 * where a new error type could be added, we list everything.
-                 */
-                InternalConnectionResult.Failure.BleNotSupported -> "BleNotSupported"
-                InternalConnectionResult.Failure.ConnectionTimeout -> "ConnectionTimeout"
-                InternalConnectionResult.Failure.DeviceConnectivityBluetoothDisabled -> "DeviceConnectivityBluetoothDisabled"
-                InternalConnectionResult.Failure.DeviceConnectivityLocationDisabled -> "DeviceConnectivityLocationDisabled"
-                InternalConnectionResult.Failure.DeviceNotFound -> "DeviceNotFound"
-                InternalConnectionResult.Failure.InitializingFailed -> "InitializingFailed"
-                InternalConnectionResult.Failure.InternalState -> "InternalState"
-                InternalConnectionResult.Failure.NoDeviceAddress -> "NoDeviceAddress"
-                InternalConnectionResult.Failure.PairingFailed -> "PairingFailed"
-                InternalConnectionResult.Failure.PermissionNotGranted -> "PermissionNotGranted"
-                InternalConnectionResult.Failure.ServiceNotFound -> "ServiceNotFound"
-                is InternalConnectionResult.Failure.Unknown -> "UnknownError: ${this.error.msg}"
-            })
-            return map
+            Arguments.createMap().apply {
+                putString("error", when (error) {
+                    /**
+                     * Most of these errors are for the BLE lib so it does not really make sense
+                     * to have them here but for the sake of being explicit and not using an "else"
+                     * where a new error type could be added, we list everything.
+                     */
+                    InternalConnectionResult.Failure.BleNotSupported -> "BleNotSupported"
+                    InternalConnectionResult.Failure.ConnectionTimeout -> "ConnectionTimeout"
+                    InternalConnectionResult.Failure.DeviceConnectivityBluetoothDisabled -> "DeviceConnectivityBluetoothDisabled"
+                    InternalConnectionResult.Failure.DeviceConnectivityLocationDisabled -> "DeviceConnectivityLocationDisabled"
+                    InternalConnectionResult.Failure.DeviceNotFound -> "DeviceNotFound"
+                    InternalConnectionResult.Failure.InitializingFailed -> "InitializingFailed"
+                    InternalConnectionResult.Failure.InternalState -> "InternalState"
+                    InternalConnectionResult.Failure.NoDeviceAddress -> "NoDeviceAddress"
+                    InternalConnectionResult.Failure.PairingFailed -> "PairingFailed"
+                    InternalConnectionResult.Failure.PermissionNotGranted -> "PermissionNotGranted"
+                    InternalConnectionResult.Failure.ServiceNotFound -> "ServiceNotFound"
+                    is InternalConnectionResult.Failure.Unknown -> "UnknownError: ${error.msg}"
+                })
+                putBoolean("success", false)
+            }
         }
     }
-}
 
 internal fun SendApduResult.toWritableMap(): WritableMap {
     when(this) {
         is SendApduResult.Success -> {
-            val map = Arguments.createMap()
-            map.putBoolean("success", true)
-            map.putString("apdu", Base64.encodeToString(this.apdu, Base64.DEFAULT))
-            return map
+            return Arguments.createMap().apply {
+                putBoolean("success", true)
+                putString("apdu", Base64.encodeToString(apdu, Base64.DEFAULT))
+            }
         }
         is SendApduResult.Failure -> {
-            val map = Arguments.createMap()
-            map.putBoolean("success", false)
-            map.putString("error", when (this.reason) {
-                SendApduFailureReason.ApduNotWellFormatted -> "ApduNotWellFormatted"
-                SendApduFailureReason.DeviceBusy -> "DeviceBusy"
-                SendApduFailureReason.DeviceLocked -> "DeviceLocked"
-                SendApduFailureReason.DeviceNotFound -> "DeviceNotFound"
-                SendApduFailureReason.NoResponse -> "NoResponse"
-                SendApduFailureReason.NoUsbEndpointFound -> "NoUsbEndpointFound"
-                SendApduFailureReason.Unknown -> "Unknown"
-            })
-            return map
+            return Arguments.createMap().apply {
+                putBoolean("success", false)
+                putString("error", when (reason) {
+                    SendApduFailureReason.ApduNotWellFormatted -> "ApduNotWellFormatted"
+                    SendApduFailureReason.DeviceBusy -> "DeviceBusy"
+                    SendApduFailureReason.DeviceLocked -> "DeviceLocked"
+                    SendApduFailureReason.DeviceNotFound -> "DeviceNotFound"
+                    SendApduFailureReason.NoResponse -> "NoResponse"
+                    SendApduFailureReason.NoUsbEndpointFound -> "NoUsbEndpointFound"
+                    SendApduFailureReason.Unknown -> "Unknown"
+                })
+            }
         }
     }
 }
 
-internal fun TransportEvent.DeviceConnectionLost.toWritableMap(): WritableMap {
-    val map = Arguments.createMap()
-    map.putString("id", this.id)
-    return map
-}
+internal fun TransportEvent.DeviceConnectionLost.toWritableMap(): WritableMap =
+    Arguments.createMap().apply {
+        putString("id", id)
+    }
 
 /* lists */
 
-fun List<DiscoveryDevice>.toWritableArray(): WritableArray {
-    val array = Arguments.createArray()
-    this.forEach {
-        array.pushMap(it.toWritableMap())
+fun List<DiscoveryDevice>.toWritableArray(): WritableArray =
+    Arguments.createArray().apply {
+        forEach {
+            pushMap(it.toWritableMap())
+        }
     }
-    return array
-}
