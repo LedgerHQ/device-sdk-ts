a98a4f9397d3895a36a968931843664b6e2a3d0e
Olivier Freyssinet <olivier.freyssinet@ledger.fr>
2025-02-13 17:00:48 +0100

    ✨ (rn-hid): Device reconnections (State Machine)

--- packages/transport/rn-hid/android/src/main/kotlin/com/ledger/devicesdk/shared/androidMain/transport/usb/controller/UsbAttachedReceiverController.kt
+++ packages/transport/rn-hid/android/src/main/kotlin/com/ledger/devicesdk/shared/androidMain/transport/usb/controller/UsbAttachedReceiverController.kt
@@ -9,13 +9,17 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.hardware.usb.UsbDevice
 import android.hardware.usb.UsbManager
 import androidx.core.content.ContextCompat
 import com.ledger.devicesdk.shared.internal.utils.Controller
 import com.ledger.devicesdk.shared.androidMain.transport.usb.AndroidUsbTransport
+import com.ledger.devicesdk.shared.androidMain.transport.usb.model.LedgerUsbDevice
+import com.ledger.devicesdk.shared.androidMain.transport.usb.model.UsbState
+import com.ledger.devicesdk.shared.androidMain.transport.usb.utils.getAndroidUsbDevice
+import com.ledger.devicesdk.shared.androidMain.transport.usb.utils.toLedgerUsbDevice
 import timber.log.Timber
 
-@Deprecated("Disable after the migration of BLE library. Might be enable in the future")
 internal class UsbAttachedReceiverController(
     private val context: Context,
     private val androidUsbTransport: AndroidUsbTransport,
@@ -41,6 +45,16 @@ internal class UsbAttachedReceiverController(
         intent: Intent,
     ) {
         Timber.i("UsbAttachedReceiverController:onReceive")
-        // ASK TO BE REMOVED
+        val androidUsbDevice: UsbDevice = intent.getAndroidUsbDevice()
+        val ledgerUsbDevice: LedgerUsbDevice? = androidUsbDevice.toLedgerUsbDevice()
+        if (ledgerUsbDevice != null) {
+            androidUsbTransport.updateUsbState(
+                state = UsbState.Attached(
+                    ledgerUsbDevice = ledgerUsbDevice,
+                    usbDevice = androidUsbDevice,
+                )
+            )
+        }
+
     }
 }
--- packages/transport/rn-hid/android/src/main/kotlin/com/ledger/devicesdk/shared/androidMain/transport/usb/controller/UsbDetachedReceiverController.kt
+++ packages/transport/rn-hid/android/src/main/kotlin/com/ledger/devicesdk/shared/androidMain/transport/usb/controller/UsbDetachedReceiverController.kt
@@ -9,12 +9,14 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.hardware.usb.UsbDevice
 import android.hardware.usb.UsbManager
 import androidx.core.content.ContextCompat
 import com.ledger.devicesdk.shared.androidMain.transport.usb.AndroidUsbTransport
+import com.ledger.devicesdk.shared.androidMain.transport.usb.model.LedgerUsbDevice
 import com.ledger.devicesdk.shared.androidMain.transport.usb.model.UsbState
 import com.ledger.devicesdk.shared.androidMain.transport.usb.utils.getAndroidUsbDevice
-import com.ledger.devicesdk.shared.androidMain.transport.usb.utils.toUsbDevice
+import com.ledger.devicesdk.shared.androidMain.transport.usb.utils.toLedgerUsbDevice
 import com.ledger.devicesdk.shared.internal.utils.Controller
 import timber.log.Timber
 
@@ -43,9 +45,15 @@ internal class UsbDetachedReceiverController(
         intent: Intent,
     ) {
         Timber.i("UsbDetachedReceiverController:onReceive")
-        val usbDevice = intent.getAndroidUsbDevice().toUsbDevice()
-        if (usbDevice != null) {
-            androidUsbTransport.updateUsbState(state = UsbState.Detached(device = usbDevice))
+        val usbDevice: UsbDevice = intent.getAndroidUsbDevice()
+        val ledgerUsbDevice: LedgerUsbDevice? = usbDevice.toLedgerUsbDevice()
+        if (ledgerUsbDevice != null) {
+            androidUsbTransport.updateUsbState(
+                state = UsbState.Detached(
+                    ledgerUsbDevice = ledgerUsbDevice,
+                    usbDevice = usbDevice,
+                )
+            )
         }
     }
 }
