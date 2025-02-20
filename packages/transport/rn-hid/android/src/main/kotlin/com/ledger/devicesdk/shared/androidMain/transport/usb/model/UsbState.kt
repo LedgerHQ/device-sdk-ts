6a2eba8f13b26800ab64b2d64dc06c72b62a68c8
ofreyssinet-ledger <olivier.freyssinet@ledger.fr>
2025-02-20 12:56:50 +0100

    ⚰️ (rn-hid): PR feedback: remove dead code

--- packages/transport/rn-hid/android/src/main/kotlin/com/ledger/devicesdk/shared/androidMain/transport/usb/model/UsbState.kt
+++ packages/transport/rn-hid/android/src/main/kotlin/com/ledger/devicesdk/shared/androidMain/transport/usb/model/UsbState.kt
@@ -8,11 +8,9 @@ package com.ledger.devicesdk.shared.androidMain.transport.usb.model
 internal sealed class UsbState {
     data class Detached(
         val ledgerUsbDevice: LedgerUsbDevice,
-        val usbDevice: android.hardware.usb.UsbDevice,
     ) : UsbState()
 
     data class Attached(
         val ledgerUsbDevice: LedgerUsbDevice,
-        val usbDevice: android.hardware.usb.UsbDevice,
     ): UsbState()
 }
