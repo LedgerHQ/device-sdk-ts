98a1d6a016144df11fdd1850ecaad7fee471ce87
Mathieu Bertin <mathieu.bertin@ledger.fr>
2025-02-26 21:27:15 -0700

    🚨 (doc): Fix prettier for documentation

--- apps/docs/pages/docs/_meta.js
+++ apps/docs/pages/docs/_meta.js
@@ -3,7 +3,7 @@ export default {
     title: "Device Interaction",
     type: "separator",
   },
-  'getting-started': 'Getting started',
+  "getting-started": "Getting started",
   beginner: "Beginner's guides",
   integration: "Integration Walkthroughs",
   references: "References",
