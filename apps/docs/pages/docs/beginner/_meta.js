d43380415c79528fa9acfdfa9554d6d0e75b97e1
cfranceschi-ledger <celia.franceschi@ledger.fr>
2025-02-28 19:30:30 +0100

    📝 (doc) [NO-ISSUE]: BLE and USB identifiers doc
    Added identifiers in the reference section and short documentation in the beginners section about adding BLE and USB transport for Flex and Stax
    
    ✅ Ran prettier fix

--- apps/docs/pages/docs/beginner/_meta.js
+++ apps/docs/pages/docs/beginner/_meta.js
@@ -3,4 +3,5 @@ export default {
   init_dmk: "Initialize Device Management Kit",
   discover_and_connect: "Discover and connect",
   exchange_data: "Exchange data with the device",
+  ble_flex_stax: "Add BLE and USB support for Flex and Stax",
 };
