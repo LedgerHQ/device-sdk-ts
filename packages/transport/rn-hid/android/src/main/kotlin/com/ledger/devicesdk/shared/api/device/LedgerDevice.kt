f635689b1ba3d2698b8995f356334f3427622164
Mathieu Bertin <mathieu.bertin@ledger.fr>
2025-04-09 12:33:50 +0200

    🐛 (rn-hid): Fix scan of hid devices in bootloader mode

--- packages/transport/rn-hid/android/src/main/kotlin/com/ledger/devicesdk/shared/api/device/LedgerDevice.kt
+++ packages/transport/rn-hid/android/src/main/kotlin/com/ledger/devicesdk/shared/api/device/LedgerDevice.kt
@@ -1,62 +1,76 @@
 package com.ledger.devicesdk.shared.api.device
 
 public sealed class LedgerDevice(
-    public val name: String,
-    public val usbInfo: UsbInfo,
-    public val bleInformation: BleInformation? = null,
+        public val name: String,
+        public val usbInfo: UsbInfo,
+        public val bleInformation: BleInformation? = null,
 ) {
-    public data object Flex : LedgerDevice(
-        name = "Ledger Flex",
-        usbInfo = UsbInfo(LEDGER_USB_VENDOR_ID, "0x70"),
-        bleInformation =
-            BleInformation(
-                serviceUuid = "13d63400-2c97-3004-0000-4c6564676572",
-                notifyCharacteristicUuid = "13d63400-2c97-3004-0001-4c6564676572",
-                writeWithResponseCharacteristicUuid = "13d63400-2c97-3004-0002-4c6564676572",
-                writeWithoutResponseCharacteristicUuid = "13d63400-2c97-3004-0003-4c6564676572",
-            ),
-    )
+    public data object Flex :
+            LedgerDevice(
+                    name = "Ledger Flex",
+                    usbInfo = UsbInfo(LEDGER_USB_VENDOR_ID, "0x70", "0x0007"),
+                    bleInformation =
+                            BleInformation(
+                                    serviceUuid = "13d63400-2c97-3004-0000-4c6564676572",
+                                    notifyCharacteristicUuid =
+                                            "13d63400-2c97-3004-0001-4c6564676572",
+                                    writeWithResponseCharacteristicUuid =
+                                            "13d63400-2c97-3004-0002-4c6564676572",
+                                    writeWithoutResponseCharacteristicUuid =
+                                            "13d63400-2c97-3004-0003-4c6564676572",
+                            ),
+            )
 
-    public data object Stax : LedgerDevice(
-        name = "Ledger Stax",
-        usbInfo = UsbInfo(LEDGER_USB_VENDOR_ID, "0x60"),
-        bleInformation =
-            BleInformation(
-                serviceUuid = "13d63400-2c97-6004-0000-4c6564676572",
-                notifyCharacteristicUuid = "13d63400-2c97-6004-0001-4c6564676572",
-                writeWithResponseCharacteristicUuid = "13d63400-2c97-6004-0002-4c6564676572",
-                writeWithoutResponseCharacteristicUuid = "13d63400-2c97-6004-0003-4c6564676572",
-            ),
-    )
+    public data object Stax :
+            LedgerDevice(
+                    name = "Ledger Stax",
+                    usbInfo = UsbInfo(LEDGER_USB_VENDOR_ID, "0x60", "0x0006"),
+                    bleInformation =
+                            BleInformation(
+                                    serviceUuid = "13d63400-2c97-6004-0000-4c6564676572",
+                                    notifyCharacteristicUuid =
+                                            "13d63400-2c97-6004-0001-4c6564676572",
+                                    writeWithResponseCharacteristicUuid =
+                                            "13d63400-2c97-6004-0002-4c6564676572",
+                                    writeWithoutResponseCharacteristicUuid =
+                                            "13d63400-2c97-6004-0003-4c6564676572",
+                            ),
+            )
 
-    public data object NanoX : LedgerDevice(
-        name = "Nano X",
-        usbInfo = UsbInfo(LEDGER_USB_VENDOR_ID, "0x40"),
-        bleInformation =
-            BleInformation(
-                serviceUuid = "13d63400-2c97-0004-0000-4c6564676572",
-                notifyCharacteristicUuid = "13d63400-2c97-0004-0001-4c6564676572",
-                writeWithResponseCharacteristicUuid = "13d63400-2c97-0004-0002-4c6564676572",
-                writeWithoutResponseCharacteristicUuid = "13d63400-2c97-0004-0003-4c6564676572",
-            ),
-    )
+    public data object NanoX :
+            LedgerDevice(
+                    name = "Nano X",
+                    usbInfo = UsbInfo(LEDGER_USB_VENDOR_ID, "0x40", "0x0004"),
+                    bleInformation =
+                            BleInformation(
+                                    serviceUuid = "13d63400-2c97-0004-0000-4c6564676572",
+                                    notifyCharacteristicUuid =
+                                            "13d63400-2c97-0004-0001-4c6564676572",
+                                    writeWithResponseCharacteristicUuid =
+                                            "13d63400-2c97-0004-0002-4c6564676572",
+                                    writeWithoutResponseCharacteristicUuid =
+                                            "13d63400-2c97-0004-0003-4c6564676572",
+                            ),
+            )
 
-    public data object NanoSPlus : LedgerDevice(
-        name = "Nano S Plus",
-        usbInfo = UsbInfo(LEDGER_USB_VENDOR_ID, "0x50"),
-        bleInformation = null,
-    )
+    public data object NanoSPlus :
+            LedgerDevice(
+                    name = "Nano S Plus",
+                    usbInfo = UsbInfo(LEDGER_USB_VENDOR_ID, "0x50", "0x0005"),
+                    bleInformation = null,
+            )
 
-    public data object NanoS : LedgerDevice(
-        name = "Nano S",
-        usbInfo = UsbInfo(LEDGER_USB_VENDOR_ID, "0x10"),
-        bleInformation = null,
-    )
+    public data object NanoS :
+            LedgerDevice(
+                    name = "Nano S",
+                    usbInfo = UsbInfo(LEDGER_USB_VENDOR_ID, "0x10", "0x0001"),
+                    bleInformation = null,
+            )
 
     public companion object {
         public const val LEDGER_USB_VENDOR_ID: String = "0x2c97"
 
-        //Cannot use reflexion here to get all subclasses as it depends of the JVM
+        // Cannot use reflexion here to get all subclasses as it depends of the JVM
         private val subclasses = buildList {
             add(Flex)
             add(Stax)
@@ -69,6 +83,7 @@ public sealed class LedgerDevice(
             return subclasses
         }
 
-        public fun getAllDevicesWithBluetooth(): List<LedgerDevice> = getAllDevices().filter { it.bleInformation != null }
+        public fun getAllDevicesWithBluetooth(): List<LedgerDevice> =
+                getAllDevices().filter { it.bleInformation != null }
     }
 }
--- packages/transport/rn-hid/android/src/main/kotlin/com/ledger/devicesdk/shared/api/device/UsbInfo.kt
+++ packages/transport/rn-hid/android/src/main/kotlin/com/ledger/devicesdk/shared/api/device/UsbInfo.kt
@@ -1,6 +1,7 @@
 package com.ledger.devicesdk.shared.api.device
 
 public data class UsbInfo(
-    val vendorId: String,
-    val productIdMask: String,
+        val vendorId: String,
+        val productIdMask: String,
+        val bootloaderProductId: String,
 )
