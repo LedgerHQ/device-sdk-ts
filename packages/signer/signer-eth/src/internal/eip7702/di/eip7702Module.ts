a52becb2f421dea25d23669efed81c62f247531b
Pierre Aoun <pierre.aoun@ledger.fr>
2025-05-06 11:21:41 +0200

    :arrow_up: (signer-eth): Upgrade ethers to an eip7702 compatible version

--- packages/signer/signer-eth/src/internal/eip7702/di/eip7702Module.ts
+++ packages/signer/signer-eth/src/internal/eip7702/di/eip7702Module.ts
@@ -4,18 +4,8 @@ import { eip7702Types } from "@internal/eip7702/di/eip7702Types";
 import { SignDelegationAuthorizationUseCase } from "@internal/eip7702/use-case/SignDelegationAuthorizationUseCase";
 
 export const eip7702ModuleFactory = () =>
-  new ContainerModule(
-    (
-      bind,
-      _unbind,
-      _isBound,
-      _rebind,
-      _unbindAsync,
-      _onActivation,
-      _onDeactivation,
-    ) => {
-      bind(eip7702Types.SignDelegationAuthorizationUseCase).to(
-        SignDelegationAuthorizationUseCase,
-      );
-    },
-  );
+  new ContainerModule(({ bind }) => {
+    bind(eip7702Types.SignDelegationAuthorizationUseCase).to(
+      SignDelegationAuthorizationUseCase,
+    );
+  });
