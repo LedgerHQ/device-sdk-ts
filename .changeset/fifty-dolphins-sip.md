66af3f97d824cca2b30c91f942235758fa915b1e
Valentin D. Pinkman <valentin.dealmeida@ledger.fr>
2025-05-12 17:55:56 +0200

    🌱 (keyring): Setup basic files and di

---
+++ .changeset/fifty-dolphins-sip.md
@@ -0,0 +1,5 @@
+---
+"@ledgerhq/device-app-kit-ledger-keyring-protocol": patch
+---
+
+Setup seed files for ledger-keyring-protocol trusted app
--- packages/trusted-apps/ledger-keyring-protocol/vitest.config.mjs
+++ packages/trusted-apps/ledger-keyring-protocol/vitest.config.mjs
@@ -6,11 +6,19 @@ export default defineConfig({
   test: {
     ...baseConfig.test,
     include: ["src/**/*.test.ts"],
+    setupFiles: ["./vitest.setup.mjs"],
     coverage: {
       reporter: ["lcov", "text"],
       provider: "istanbul",
       include: ["src/**/*.ts"],
       exclude: ["src/**/*.stub.ts", "src/index.ts"],
+    },
+  },
+  resolve: {
+    alias: {
+      "@api": path.resolve(__dirname, "src/api"),
+      "@internal": path.resolve(__dirname, "src/internal"),
+      "@root": path.resolve(__dirname, "."),
     },
   },
 });
