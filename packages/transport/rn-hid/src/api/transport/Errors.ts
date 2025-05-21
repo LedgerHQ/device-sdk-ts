66acdec679fd594ead33f9f32c19d5075c53bc8c
Olivier Freyssinet <olivier.freyssinet@ledger.fr>
2025-05-21 11:34:55 +0200

    🐛 (rn-hid): Remap DeviceDisconnected error

--- packages/transport/rn-hid/src/api/transport/Errors.ts
+++ packages/transport/rn-hid/src/api/transport/Errors.ts
@@ -1,6 +1,6 @@
 import { GeneralDmkError } from "@ledgerhq/device-management-kit";
 
-export class SendApduError extends GeneralDmkError {
+export class HidTransportSendApduUnknownError extends GeneralDmkError {
   override readonly _tag = "HidTransportSendApduUnknownError";
   constructor(readonly err?: unknown) {
     super(err);
