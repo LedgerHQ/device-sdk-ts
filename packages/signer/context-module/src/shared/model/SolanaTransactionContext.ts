1fcd449ebc8935c8786acc8a417c55b5ad3a7b45
fAnselmi-Ledger <francesco.anselmi@ledger.fr>
2025-06-05 15:12:46 +0200

    ✨ (solana-signer-and-context-module): Add BuildTransactionContextTask and context module getSolanaContext

---
+++ packages/signer/context-module/src/shared/model/SolanaTransactionContext.ts
@@ -0,0 +1,21 @@
+import { type DeviceModelId } from "@ledgerhq/device-management-kit";
+
+import { type PkiCertificate } from "@/pki/model/PkiCertificate";
+
+export type SolanaTransactionContext = {
+  challenge: string | undefined;
+  deviceModelId: DeviceModelId;
+  tokenAddress?: string;
+  createATA?: {
+    address: string;
+    mintAddress: string;
+  };
+};
+
+export type SolanaTransactionContextResult = {
+  descriptor: Uint8Array;
+  certificate: PkiCertificate;
+  tokenAccount: string;
+  owner: string;
+  contract: string;
+};
