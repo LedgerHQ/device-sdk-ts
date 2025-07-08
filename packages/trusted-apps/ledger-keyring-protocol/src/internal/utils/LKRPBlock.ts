7a155a30627e379cf7c4198d9b1a4b918b25cb30
Theophile Sandoz <theophile.sandoz@ledger.fr>
2025-07-08 17:23:11 +0200

    ✨ (keyring): Implement the LKRPBlock class

---
+++ packages/trusted-apps/ledger-keyring-protocol/src/internal/utils/LKRPBlock.ts
@@ -0,0 +1,84 @@
+import { Either, Just, type Maybe, Nothing, Right } from "purify-ts";
+
+import { type LKRPParsingError } from "@api/app-binder/Errors";
+
+import { bytesToHex, hexToBytes } from "./hex";
+import { TLVBuilder } from "./TLVBuilder";
+import { TLVParser } from "./TLVParser";
+import { type LKRPBlockData } from "./types";
+
+export class LKRPBlock {
+  private hashValue: Maybe<Promise<string>> = Nothing; // Cached hash value for performance
+  private data: Maybe<Either<LKRPParsingError, LKRPBlockData>>;
+
+  public constructor(
+    private readonly bytes: Uint8Array,
+    data?: LKRPBlockData,
+  ) {
+    this.data = data ? Just(Right(data)) : Nothing;
+  }
+
+  static fromHex(hex: string): LKRPBlock {
+    return new LKRPBlock(hexToBytes(hex));
+  }
+
+  static fromData(data: LKRPBlockData): LKRPBlock {
+    const bytes = new TLVBuilder()
+      .addInt(1, 1) // Version 1
+      .addHash(hexToBytes(data.parent))
+      .addPublicKey(data.issuer)
+      .addInt(data.commands.length, 1)
+      .with((builder) =>
+        data.commands.forEach((cmd) => builder.push(cmd.toU8A())),
+      )
+      .addSignature(data.signature)
+      .build();
+
+    return new LKRPBlock(bytes, data);
+  }
+
+  toString(): string {
+    return bytesToHex(this.bytes);
+  }
+
+  toU8A(): Uint8Array {
+    return this.bytes;
+  }
+
+  parse(): Either<LKRPParsingError, LKRPBlockData> {
+    return this.data.orDefaultLazy(() => {
+      const data = new TLVParser(this.bytes).parseBlockData();
+      this.data = Just(data);
+      return data;
+    });
+  }
+
+  toHuman(): Either<LKRPParsingError, string> {
+    return this.parse()
+      .chain((data) =>
+        Either.sequence(data.commands.map((cmd) => cmd.toHuman())).map(
+          (commands) => ({ ...data, commands }),
+        ),
+      )
+      .map((data) =>
+        [
+          `Parent: ${data.parent}`,
+          `Issuer: ${bytesToHex(data.issuer)}`,
+          `Commands:${data.commands
+            .flatMap((cmd) => cmd.split("\n").map((l) => `\n  ${l}`))
+            .join("")}`,
+          `Signature: ${bytesToHex(data.signature)}`,
+        ].join("\n"),
+      );
+  }
+
+  hash(): Promise<string> {
+    return this.hashValue.orDefaultLazy(() => {
+      const hashValue = crypto.subtle
+        .digest("SHA-256", this.bytes)
+        .then((buffer) => bytesToHex(new Uint8Array(buffer)));
+      this.hashValue = Just(hashValue);
+      return hashValue;
+    });
+  }
+}
