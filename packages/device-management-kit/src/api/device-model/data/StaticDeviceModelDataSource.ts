e716b4ca83b700a44ed13f99f96aa1e046cd0e62
Olivier Freyssinet <olivier.freyssinet@ledger.fr>
2025-07-09 15:50:08 +0200

    ✨ (dmk): Support Apex

--- packages/device-management-kit/src/api/device-model/data/StaticDeviceModelDataSource.ts
+++ packages/device-management-kit/src/api/device-model/data/StaticDeviceModelDataSource.ts
@@ -87,6 +87,24 @@ export class StaticDeviceModelDataSource implements DeviceModelDataSource {
         },
       ],
     }),
+    [DeviceModelId.APEX]: new TransportDeviceModel({
+      id: DeviceModelId.APEX,
+      productName: "Ledger Apex",
+      usbProductId: 0x80,
+      bootloaderUsbProductId: 0x0008,
+      usbOnly: false,
+      memorySize: 1533 * 1024,
+      blockSize: 32,
+      masks: [0x33400000],
+      bluetoothSpec: [
+        {
+          serviceUuid: "13d63400-2c97-6004-0000-4c6564676572", // WILL CHANGE, for now same as Stax
+          notifyUuid: "13d63400-2c97-6004-0001-4c6564676572", // WILL CHANGE, for now same as Stax
+          writeUuid: "13d63400-2c97-6004-0002-4c6564676572", // WILL CHANGE, for now same as Stax
+          writeCmdUuid: "13d63400-2c97-6004-0003-4c6564676572", // WILL CHANGE, for now same as Stax
+        },
+      ],
+    }),
   };
 
   getAllDeviceModels(): TransportDeviceModel[] {
