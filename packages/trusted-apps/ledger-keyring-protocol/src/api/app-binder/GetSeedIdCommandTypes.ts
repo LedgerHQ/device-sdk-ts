0e6335e1fac076575add9e1411ee797252b84993
Theophile Sandoz <theophile.sandoz@ledger.fr>
2025-07-11 09:56:24 +0200

    ♻️ (keyring): Fix the parsing of GET_SEED_ID

--- packages/trusted-apps/ledger-keyring-protocol/src/api/app-binder/GetSeedIdCommandTypes.ts
+++ packages/trusted-apps/ledger-keyring-protocol/src/api/app-binder/GetSeedIdCommandTypes.ts
@@ -1,12 +1,13 @@
-export type GetSeedIdCommandResponse = Partial<{
-  readonly pubKeyHeader: Uint8Array;
-  readonly pubKey: Uint8Array;
-  readonly pubKeySig: Uint8Array;
-  readonly attestationId: Uint8Array;
-  readonly attestationHeader: Uint8Array;
-  readonly attestationKey: Uint8Array;
-  readonly attestationSig: Uint8Array;
-}>;
+export type GetSeedIdCommandResponse = {
+  readonly credential: {
+    readonly version: number;
+    readonly curveId: number;
+    readonly signAlgorithm: number;
+    readonly publicKey: string;
+  };
+  readonly signature: string;
+  readonly attestation: string;
+};
 
 export type GetSeedIdCommandArgs = {
   readonly challengeTLV: string;
