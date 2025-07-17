8a2934aea273c3eb2d6c6153a91f428f9505ec1f
Theophile Sandoz <theophile.sandoz@ledger.fr>
2025-07-17 12:27:30 +0200

    📝 (keyring): Update sample app to test KeypairAuth

---
+++ apps/sample/src/utils/crypto.ts
@@ -0,0 +1,17 @@
+import * as secp from "@noble/secp256k1";
+
+export function randomPrivateKey() {
+  return bytesToHex(secp.utils.randomPrivateKey());
+}
+
+export function getPublicKey(privateKey: string) {
+  return secp.getPublicKey(privateKey);
+}
+
+export function hexToBytes(hex: string): Uint8Array {
+  return Uint8Array.from(Buffer.from(hex, "hex"));
+}
+
+export function bytesToHex(bytes: Uint8Array): string {
+  return Buffer.from(bytes).toString("hex");
+}
