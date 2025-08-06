8157e04eb03c26ae030e13492aba1b4a343e37fa
Olivier Freyssinet <olivier.freyssinet@ledger.fr>
2025-08-06 11:18:42 +0200

    🐛 (config): Fix tsconfig and eslint config

--- packages/transport/rn-hid/eslint.config.mjs
+++ packages/transport/rn-hid/eslint.config.mjs
@@ -3,7 +3,6 @@ import config from "@ledgerhq/eslint-config-dsdk";
 export default [
   ...config,
   {
-    ignores: ["eslint.config.mjs", "lib/*", "vitest.*.mjs"],
     languageOptions: {
       parserOptions: {
         project: "./tsconfig.json",
