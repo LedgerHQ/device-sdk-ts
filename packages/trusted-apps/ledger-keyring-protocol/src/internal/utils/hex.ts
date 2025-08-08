820aa178db015b3ca2659e3b831194a889b8ab31
Theophile Sandoz <theophile.sandoz@ledger.fr>
2025-08-08 10:21:37 +0200

    🐛 (keyring): Remove dependency to Buffer

--- packages/trusted-apps/ledger-keyring-protocol/src/internal/utils/hex.ts
+++ packages/trusted-apps/ledger-keyring-protocol/src/internal/utils/hex.ts
@@ -1,7 +1,12 @@
+import {
+  bufferToHexaString,
+  hexaStringToBuffer,
+} from "@ledgerhq/device-management-kit";
+
 export function hexToBytes(hex: string): Uint8Array {
-  return Uint8Array.from(Buffer.from(hex, "hex"));
+  return hexaStringToBuffer(hex) ?? new Uint8Array();
 }
 
 export function bytesToHex(bytes: Uint8Array): string {
-  return Buffer.from(bytes).toString("hex");
+  return bufferToHexaString(bytes).slice(2); // Remove the "0x" prefix
 }
