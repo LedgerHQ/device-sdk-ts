fa95d752c57d811704ea2c44651fb68b8e88e6d9
Theophile Sandoz <theophile.sandoz@ledger.fr>
2025-09-05 17:54:57 +0200

    🐛 (keyring): Fix `OWNER & ~CAN_ADD_BLOCK` support

--- packages/trusted-apps/ledger-keyring-protocol/src/internal/utils/LKRPCommand.test.ts
+++ packages/trusted-apps/ledger-keyring-protocol/src/internal/utils/LKRPCommand.test.ts
@@ -1,11 +1,152 @@
 import { Just, Left, Nothing, Right } from "purify-ts";
 
+import { Permissions } from "@api/index";
 import { LKRPParsingError } from "@api/model/Errors";
 import { CommandTags, GeneralTags } from "@internal/models/Tags";
 
 import { LKRPCommand } from "./LKRPCommand";
 
 describe("LKRPCommand", () => {
+  describe("fromData", () => {
+    it("should encode add member permissions correctly", () => {
+      // GIVEN
+      const publicKey = Uint8Array.from([
+        0x02, 0xbf, 0x5c, 0x0d, 0x9b, 0xf0, 0xf4, 0x95, 0x16, 0x28, 0x5a, 0xd2,
+        0x03, 0x92, 0xc4, 0xc4, 0xb8, 0x71, 0x14, 0xf6, 0xc4, 0x8c, 0x84, 0x73,
+        0x06, 0xde, 0x1f, 0xe7, 0xf3, 0x93, 0x4e, 0x43, 0xc1,
+      ]);
+      // WHEN
+      const addOwner = LKRPCommand.fromData({
+        type: CommandTags.AddMember,
+        name: "OWNER",
+        publicKey,
+        permissions: Permissions.OWNER,
+      });
+      const addReader = LKRPCommand.fromData({
+        type: CommandTags.AddMember,
+        name: "READER",
+        publicKey,
+        permissions: Permissions.CAN_ENCRYPT,
+      });
+      const dapp = LKRPCommand.fromData({
+        type: CommandTags.AddMember,
+        name: "DAPP",
+        publicKey,
+        permissions: Permissions.OWNER & ~Permissions.CAN_ADD_BLOCK,
+      });
+      // THEN
+      expect(addOwner.toString()).toBe(
+        [
+          "11 30", // header
+          "04 05 4f 57 4e 45 52", // name
+          "06 21 02 bf 5c 0d 9b f0 f4 95 16 28 5a d2 03 92 c4 c4 b8 71 14 f6 c4 8c 84 73 06 de 1f e7 f3 93 4e 43 c1", // public key
+          "01 04 ff ff ff ff", // permissions (OWNER)
+        ]
+          .join("")
+          .replace(/ /g, ""),
+      );
+      expect(addReader.toString()).toBe(
+        [
+          "11 31", // header
+          "04 06 52 45 41 44 45 52", // name
+          "06 21 02 bf 5c 0d 9b f0 f4 95 16 28 5a d2 03 92 c4 c4 b8 71 14 f6 c4 8c 84 73 06 de 1f e7 f3 93 4e 43 c1", // public key
+          "01 04 00 00 00 01", // permissions (CAN_ENCRYPT)
+        ]
+          .join("")
+          .replace(/ /g, ""),
+      );
+      expect(dapp.toString()).toBe(
+        [
+          "11 2f", // header
+          "04 04 44 41 50 50", // name
+          "06 21 02 bf 5c 0d 9b f0 f4 95 16 28 5a d2 03 92 c4 c4 b8 71 14 f6 c4 8c 84 73 06 de 1f e7 f3 93 4e 43 c1", // public key
+          "01 04 ff ff ff fb", // permissions (OWNER & ~CAN_ADD_BLOCK)
+        ]
+          .join("")
+          .replace(/ /g, ""),
+      );
+    });
+  });
+
+  describe("bytesFromUnsignedData", () => {
+    it("should encode add member permissions correctly", () => {
+      // GIVEN
+      const publicKey = Uint8Array.from([
+        0x02, 0xbf, 0x5c, 0x0d, 0x9b, 0xf0, 0xf4, 0x95, 0x16, 0x28, 0x5a, 0xd2,
+        0x03, 0x92, 0xc4, 0xc4, 0xb8, 0x71, 0x14, 0xf6, 0xc4, 0x8c, 0x84, 0x73,
+        0x06, 0xde, 0x1f, 0xe7, 0xf3, 0x93, 0x4e, 0x43, 0xc1,
+      ]);
+      // WHEN
+      const addOwner = LKRPCommand.bytesFromUnsignedData({
+        type: CommandTags.AddMember,
+        name: "OWNER",
+        publicKey,
+        permissions: Permissions.OWNER,
+      });
+      const addReader = LKRPCommand.bytesFromUnsignedData({
+        type: CommandTags.AddMember,
+        name: "READER",
+        publicKey,
+        permissions: Permissions.CAN_ENCRYPT,
+      });
+      const dapp = LKRPCommand.bytesFromUnsignedData({
+        type: CommandTags.AddMember,
+        name: "DAPP",
+        publicKey,
+        permissions: Permissions.OWNER & ~Permissions.CAN_ADD_BLOCK,
+      });
+      // THEN
+      expect(addOwner).toEqual(
+        Uint8Array.from(
+          [
+            [0x11, 0x30], // header
+            [0x04, 0x05, 0x4f, 0x57, 0x4e, 0x45, 0x52], // name
+            [
+              // public key
+              0x06, 0x21, 0x02, 0xbf, 0x5c, 0x0d, 0x9b, 0xf0, 0xf4, 0x95, 0x16,
+              0x28, 0x5a, 0xd2, 0x03, 0x92, 0xc4, 0xc4, 0xb8, 0x71, 0x14, 0xf6,
+              0xc4, 0x8c, 0x84, 0x73, 0x06, 0xde, 0x1f, 0xe7, 0xf3, 0x93, 0x4e,
+              0x43, 0xc1,
+            ],
+            [0x01, 0x04, 0xff, 0xff, 0xff, 0xff], // permissions (OWNER)
+          ].flat(),
+        ),
+      );
+      expect(addReader).toEqual(
+        Uint8Array.from(
+          [
+            [0x11, 0x31], // header
+            [0x04, 0x06, 0x52, 0x45, 0x41, 0x44, 0x45, 0x52], // name
+            [
+              // public key
+              0x06, 0x21, 0x02, 0xbf, 0x5c, 0x0d, 0x9b, 0xf0, 0xf4, 0x95, 0x16,
+              0x28, 0x5a, 0xd2, 0x03, 0x92, 0xc4, 0xc4, 0xb8, 0x71, 0x14, 0xf6,
+              0xc4, 0x8c, 0x84, 0x73, 0x06, 0xde, 0x1f, 0xe7, 0xf3, 0x93, 0x4e,
+              0x43, 0xc1,
+            ],
+            [0x01, 0x04, 0x00, 0x00, 0x00, 0x01], // permissions (CAN_ENCRYPT)
+          ].flat(),
+        ),
+      );
+      expect(dapp).toEqual(
+        Uint8Array.from(
+          [
+            [0x11, 0x2f], // header
+            [0x04, 0x04, 0x44, 0x41, 0x50, 0x50], // name
+            [
+              // public key
+              0x06, 0x21, 0x02, 0xbf, 0x5c, 0x0d, 0x9b, 0xf0, 0xf4, 0x95, 0x16,
+              0x28, 0x5a, 0xd2, 0x03, 0x92, 0xc4, 0xc4, 0xb8, 0x71, 0x14, 0xf6,
+              0xc4, 0x8c, 0x84, 0x73, 0x06, 0xde, 0x1f, 0xe7, 0xf3, 0x93, 0x4e,
+              0x43, 0xc1,
+            ],
+            [0x01, 0x04, 0xff, 0xff, 0xff, 0xfb], // permissions (OWNER & ~CAN_ADD_BLOCK)
+          ].flat(),
+        ),
+      );
+    });
+  });
+
   describe("toString", () => {
     it("should return the hex of the bytes of the command", () => {
       // WHEN
