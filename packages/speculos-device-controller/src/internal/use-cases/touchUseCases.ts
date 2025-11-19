14371d5d665c12215a364d9b3d301f64c645e32c
Louis Aussedat <louis.aussedat@ledger.fr>
2025-11-19 10:53:19 +0100

    ♻️ (speculos): Add new use cases for sign flow

--- packages/speculos-device-controller/src/internal/use-cases/touchUseCases.ts
+++ packages/speculos-device-controller/src/internal/use-cases/touchUseCases.ts
@@ -15,3 +15,33 @@ export const tapQuick =
   <K extends string>(touch: TouchController<K>, deviceKey: K) =>
   async (point: PercentCoordinates) =>
     await touch.tapAndRelease(deviceKey, point);
+
+export const sign =
+  <K extends string>(touch: TouchController<K>, deviceKey: K) =>
+  async () =>
+    await tapLong(touch, deviceKey)({ x: 85, y: 80 });
+
+export const reject =
+  <K extends string>(touch: TouchController<K>, deviceKey: K) =>
+  async () =>
+    await tapQuick(touch, deviceKey)({ x: 20, y: 90 });
+
+export const navigateNext =
+  <K extends string>(touch: TouchController<K>, deviceKey: K) =>
+  async () =>
+    await tapQuick(touch, deviceKey)({ x: 90, y: 90 });
+
+export const navigatePrevious =
+  <K extends string>(touch: TouchController<K>, deviceKey: K) =>
+  async () =>
+    await tapQuick(touch, deviceKey)({ x: 45, y: 90 });
+
+export const mainButton =
+  <K extends string>(touch: TouchController<K>, deviceKey: K) =>
+  async () =>
+    await tapQuick(touch, deviceKey)({ x: 50, y: 80 });
+
+export const secondaryButton =
+  <K extends string>(touch: TouchController<K>, deviceKey: K) =>
+  async () =>
+    await tapQuick(touch, deviceKey)({ x: 50, y: 90 });
