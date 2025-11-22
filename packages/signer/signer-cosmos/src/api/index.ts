6bf524069189cbed9f838aeaf998927f1c7939f5
Peter Anyaogu <peter@noble.xyz>
2025-11-21 23:33:28 -0300

    feat(signer-cosmos): add type exports and reflect-metadata import
    Add type exports for address and transaction operations from device action types
    Import reflect-metadata for inversify dependency injection support

---
+++ packages/signer/signer-cosmos/src/api/index.ts
@@ -0,0 +1,16 @@
+export type {
+  GetAddressDAError,
+  GetAddressDAIntermediateValue,
+  GetAddressDAOutput,
+  GetAddressDAReturnType,
+} from "@api/app-binder/GetAddressDeviceActionTypes";
+export type {
+  SignTransactionDAError,
+  SignTransactionDAIntermediateValue,
+  SignTransactionDAOutput,
+  SignTransactionDAReturnType,
+} from "@api/app-binder/SignTransactionDeviceActionTypes";
+export * from "@api/model/AddressOptions";
+export * from "@api/model/Signature";
+export * from "@api/model/SignDoc";
+export * from "@api/model/TransactionOptions";
