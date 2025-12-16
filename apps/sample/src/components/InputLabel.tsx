755ea3cdfc9885b0ac3435dab80c27ed22a72220
Olivier Freyssinet <olivier.freyssinet@ledger.fr>
2025-12-16 14:56:47 +0100

    💄 (sample): InputLabel pixel polish

--- apps/sample/src/components/InputLabel.tsx
+++ apps/sample/src/components/InputLabel.tsx
@@ -2,12 +2,11 @@ import { Text } from "@ledgerhq/react-ui";
 import styled from "styled-components";
 
 export const InputLabel = styled(Text).attrs({
-  fontSize: "12px",
+  fontSize: "13px",
   alignSelf: "center",
   flexShrink: 0,
   color: "neutral.c70",
   ml: 6,
-  paddingTop: "1px",
 })``;
 
 export const SelectInputLabel = styled(InputLabel).attrs({
