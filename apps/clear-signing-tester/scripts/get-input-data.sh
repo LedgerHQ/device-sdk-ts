021b46edd9fba79712c6401075c05cc0142773ec
Louis Aussedat <louis.aussedat@ledger.fr>
2026-01-12 11:22:44 +0100

    ♻️ (cs-tester): Improve etherscan helpers

--- apps/clear-signing-tester/scripts/get-input-data.sh
+++ apps/clear-signing-tester/scripts/get-input-data.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 
 curl -s "https://etherscan.io/tx/$1" | grep -o 'Function:.*' | sed 's/Function://' | sed 's/(.*)//g'
--- apps/clear-signing-tester/scripts/get-rawtx.sh
+++ apps/clear-signing-tester/scripts/get-rawtx.sh
@@ -1,1 +1,3 @@
-curl -s "https://arbiscan.io/getRawTx?tx=$1" | grep "Returned Raw Transaction Hex" | grep -o '0x[0-9a-fA-F]*'
+#!/bin/bash
+
+curl -s "https://etherscan.io/getRawTx?tx=$1" | grep "Returned Raw Transaction Hex" | grep -o '0x[0-9a-fA-F]*'
