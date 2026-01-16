1c5b7e65c53aba67d7aa49b03818ecad837e83f2
Louis Aussedat <louis.aussedat@ledger.fr>
2026-01-16 10:25:38 +0100

    ✨ (context-module): Add setTrustedNameDataSource builder entry

--- packages/signer/context-module/src/trusted-name/di/trustedNameModuleFactory.ts
+++ packages/signer/context-module/src/trusted-name/di/trustedNameModuleFactory.ts
@@ -1,13 +1,22 @@
 import { ContainerModule } from "inversify";
 
+import { type ContextModuleConfig } from "@/config/model/ContextModuleConfig";
 import { HttpTrustedNameDataSource } from "@/trusted-name/data/HttpTrustedNameDataSource";
 import { trustedNameTypes } from "@/trusted-name/di/trustedNameTypes";
 import { TrustedNameContextFieldLoader } from "@/trusted-name/domain/TrustedNameContextFieldLoader";
 import { TrustedNameContextLoader } from "@/trusted-name/domain/TrustedNameContextLoader";
 
-export const trustedNameModuleFactory = () =>
+export const trustedNameModuleFactory = (config?: ContextModuleConfig) =>
   new ContainerModule(({ bind }) => {
-    bind(trustedNameTypes.TrustedNameDataSource).to(HttpTrustedNameDataSource);
+    if (config?.customTrustedNameDataSource) {
+      bind(trustedNameTypes.TrustedNameDataSource).toConstantValue(
+        config.customTrustedNameDataSource,
+      );
+    } else {
+      bind(trustedNameTypes.TrustedNameDataSource).to(
+        HttpTrustedNameDataSource,
+      );
+    }
     bind(trustedNameTypes.TrustedNameContextLoader).to(
       TrustedNameContextLoader,
     );
