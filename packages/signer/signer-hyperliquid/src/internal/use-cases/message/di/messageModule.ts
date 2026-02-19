31023db4190c8f8639ca26e8f6e658a6a8e6f8b4
Stéphane PROHASZKA <stephane.prohaszka@ledger.fr>
2026-02-19 10:42:25 +0100

    🚀 (hyperliquid) [LIVE-25938]: Remove unecessar sign message as actions are closer to transaction
    Signed-off-by: Stéphane PROHASZKA <stephane.prohaszka@ledger.fr>

--- packages/signer/signer-hyperliquid/src/internal/use-cases/message/di/messageModule.ts
+++
@@ -1,9 +0,0 @@
-import { ContainerModule } from "inversify";
-
-import { messageTypes } from "@internal/use-cases/message/di/messageTypes";
-import { SignMessageUseCase } from "@internal/use-cases/message/SignMessageUseCase";
-
-export const messageModuleFactory = () =>
-  new ContainerModule(({ bind }) => {
-    bind(messageTypes.SignMessageUseCase).to(SignMessageUseCase);
-  });
