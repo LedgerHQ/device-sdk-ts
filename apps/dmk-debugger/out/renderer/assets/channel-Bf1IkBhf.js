ae0cd799f2a16f787c92ac7778a7c3ef7aec6f84
fAnselmi-Ledger <francesco.anselmi@ledger.fr>
2026-04-24 12:27:12 +0200

    wip

--- apps/dmk-debugger/out/renderer/assets/channel-Bf1IkBhf.js
+++
@@ -1,7 +0,0 @@
-import { aq as Utils, ar as Color } from "./index-DNJlf-L4.js";
-const channel = (color, channel2) => {
-  return Utils.lang.round(Color.parse(color)[channel2]);
-};
-export {
-  channel as c
-};
