e9773525439044eee56e6eb6bc5d8c0567f159dc
fAnselmi-Ledger <francesco.anselmi@ledger.fr>
2026-05-05 18:00:46 +0200

    wip

--- packages/signer/context-module/src/transaction-check/scan-handlers/web3CheckScanPaths.ts
+++
@@ -1,3 +0,0 @@
-/** Path suffix after `web3checks.url` `/{family}/scan/tx`. */
-export const WEB3CHECKS_ETHEREUM_TX_SCAN_PATH = "/ethereum/scan/tx";
-export const WEB3CHECKS_SOLANA_TX_SCAN_PATH = "/solana/scan/tx";
