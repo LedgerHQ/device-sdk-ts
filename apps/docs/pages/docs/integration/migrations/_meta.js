fdc184308d7434b847aa8b9aa49b14e3636e9d08
cfranceschi-ledger <celia.franceschi@ledger.fr>
2026-06-25 17:40:53 +0200

    📝 (doc) [NO-ISSUE]: Ledgerjs -> DMK migration guide

--- apps/docs/pages/docs/integration/migrations/_meta.js
+++ apps/docs/pages/docs/integration/migrations/_meta.js
@@ -3,4 +3,5 @@ import { title } from "process";
 export default {
   dmk: "Device Management Kit",
   signers: "Device Signer Kits",
+  "ledgerjs-to-dmk": "LedgerJS → DMK",
 };
--- apps/docs/pages/docs/integration/migrations/signers/eth/_meta.js
+++ apps/docs/pages/docs/integration/migrations/signers/eth/_meta.js
@@ -1,4 +1,5 @@
 export default {
+  hw_app_eth_to_dmk: "hw-app-eth → DMK",
   "1_1_0_to_1_2_0": "v1.1.0 -> 1.2.0",
   "1_3_3_to_1_4_0": "v1.3.3 -> 1.4.0",
 };
