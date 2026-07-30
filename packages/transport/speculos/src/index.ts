80565a3605c3fc5e337077ca5db079a3392d6b6e
Louis Aussedat <louis.aussedat@ledger.fr>
2026-07-30 15:22:29 +0200

    🔬 (transport-speculos) [NO-ISSUE]: Verify CI --affected on a speculos transport change

--- packages/transport/speculos/src/index.ts
+++ packages/transport/speculos/src/index.ts
@@ -1,2 +1,4 @@
 export * from "./api/SpeculosTransport";
 export * from "./internal/datasource/HttpSpeculosDatasource";
+
+// CI verify: speculos transport change (throwaway, targets chore/dsdk-1402-ci-turbo-affected)
